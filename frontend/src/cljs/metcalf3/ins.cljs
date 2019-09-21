5ff0a1dc0cb8e639d148ff7fa8bbc05f57e9bac6
Oliver George <oliver@condense.com.au>
2019-09-21 16:24:38 +1000

    Add interceptor and sub to count form data changes
    Because
    * we need a mechanism to reset uncontrolled components
    * uncontrolled state of form fields typically relates to incomplete data entry
    * react suggests changing to key to reset these uncontrolled components
    
    Approach
    * Add an interceptor which watches for form data change and increments a counter
    * Add a sub to provide access to "tick" value
    * Let views use tick value to create :key for uncontrolled components

--- frontend/src/cljs/metcalf3/ins.cljs
+++ frontend/src/cljs/metcalf3/ins.cljs
@@ -10,4 +10,17 @@
                (js/console.log "Effect: " k v))
              ctx)))
 
-(def std-ins [rf/debug log-effects])
+(def form-ticker
+  (rf/->interceptor
+    :id ::form-ticker
+    :after (fn [ctx]
+             (let [orig-db (rf/get-coeffect ctx :db)
+                   new-db (rf/get-effect ctx :db ::not-found)
+                   form1 (get-in orig-db [:form])
+                   form2 (get-in new-db [:form])]
+               (if (and (not= new-db ::not-found)
+                        (not= form1 form2))
+                 (rf/assoc-effect ctx :db (update new-db :form/tick inc))
+                 ctx)))))
+
+(def std-ins [rf/debug log-effects form-ticker])

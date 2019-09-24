5956c78774b6b274011d2163bc967fcfe3cdff75
Bice Dibley <bice@condense.com.au>
2019-09-24 17:17:57 +1000

    Changed the version git tag

--- backend/frontend/templatetags/frontend_tags.py
+++ backend/frontend/templatetags/frontend_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from django.conf import settings
 
+import subprocess
+
 register = template.Library()
 
 
@@ -12,3 +14,16 @@ def GMAPS_API_KEY():
 @register.simple_tag
 def FRONTEND_DEV_MODE():
     return settings.FRONTEND_DEV_MODE
+
+@register.simple_tag
+def GIT_VERSION():
+    if settings.GIT_VERSION != 'local':
+        return settings.GIT_VERSION
+    try:
+        p = subprocess.Popen(["git", "show", "HEAD", "--no-patch", "--no-notes", "--date=short", '--pretty=%h %cd'],
+                             stdout=subprocess.PIPE)
+        gv = p.communicate()[0]
+        return gv.strip().decode('UTF-8')
+
+    except:
+        return '(unknown)'

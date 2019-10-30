c773d4d114606201729cdaa6fdaa54c5513e8101
Wilma Karsdorp <w.karsdorp@uq.edu.au>
2019-10-30 14:20:07 +1000

    remove migration files, update initial content

--- backend/frontend/models.py
+++ backend/frontend/models.py
@@ -8,7 +8,7 @@ class SiteContent(models.Model):
     site = models.OneToOneField(Site, on_delete=models.SET_NULL, blank=True, null=True)
     title = models.CharField(max_length=32, default="TERN")
     organisation_url = models.URLField(blank=True, null=True)
-    tag_line = models.CharField(max_length=128, default="Data Submission Tool")
+    tag_line = models.CharField(max_length=128, default="SHaRED Data Submission Tool")
     email = models.EmailField(default="esupport@tern.org.au")
     doi_uri = models.CharField(max_length=1024,default='https://doi.tern.uq.edu.au/test/index.php?r=api/create&user_id=tern.data@uq.edu.au&app_id=',
                               verbose_name="DOI Service URI",
@@ -23,14 +23,15 @@ class SiteContent(models.Model):
         help_text="This is the image used on the homepage.  It needs to " \
                   "be high res so it looks good and it needs to be fairly dark " \
                   "so that the copy has good contrast and can be easily read. We "
-                  "will optimise the size so it's not too heavy.")
+                  "will optimise the size so it's not too heavy.",
+        default='images/Hemispherical-Photography.jpg')
     guide_pdf = models.FileField(upload_to='guide', blank=True, null=True)
     roadmap_pdf = models.FileField(upload_to='guide', blank=True, null=True)
     releasenotes_url = models.URLField(null=True,verbose_name="Release notes URL",blank=True)
     portal_title = models.CharField(
         max_length=64, default="TERN Data Portal",
         help_text="Used to refer to the place where lodged data can be discovered")
-    portal_url = models.URLField(blank=True, null=True)
+    portal_url = models.URLField(blank=True, null=True, default='http://tern.org.au')
     portal_record_url = models.CharField(
         max_length=512,
         default="{{site.sitecontent.portal_url}}/portal/home?uuid={{document.uuid}}",

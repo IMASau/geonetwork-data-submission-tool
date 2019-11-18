99e8ab0a5d933626dfd67b1b7284b5162b0c0a8c
Wilma Karsdorp <w.karsdorp@uq.edu.au>
2019-11-18 13:34:29 +1000

    create shared directory for resources

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+mkdir -p /var/www/metcalf/shared/staticfiles/metcalf3/
 cp -R /var/www/metcalf/frontend/resources/public/* /var/www/metcalf/shared/staticfiles/metcalf3/
 
 exec nginx -g 'daemon off;'

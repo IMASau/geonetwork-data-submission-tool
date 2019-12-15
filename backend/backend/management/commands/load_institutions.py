d256313d966562294f88addb61cb531a673da55c
Wilma Karsdorp <w.karsdorp@uq.edu.au>
2019-12-16 09:40:44 +1000

    remove unused code

--- backend/backend/management/commands/load_institutions.py
+++ backend/backend/management/commands/load_institutions.py
@@ -137,41 +137,43 @@ class Command(BaseCommand):
             )
         return
 
-#http://linkeddata.tern.org.au/viewer/tern/id/http:/linkeddata.tern.org.au/def/Person?_view=skos&_format=application/rdf+xml
-    @staticmethod
-    def _fetch_tern_data(VocabName):
-        _pred_prefix = 'http://www.w3.org/2004/02/skos/core#'
-        _vocabServer = 'http://linkeddata.tern.org.au/viewer/tern/id/http:/linkeddata.tern.org.au/def/'
-        _query = '_view=skos&_format=application/rdf+xml'
-        url = '{base}{vocabName}?{query}'.format(base=_vocabServer,vocabName=VocabName,query=_query)
-        graph = Graph()
-        graph.parse(url, format='application/rdf+xml')
-        _type_pred = URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
-        _org_object = URIRef('http://schema.org/Organization')
-        _parent_pred = URIRef(_pred_prefix + 'broader')
-        _preflabel_pred = URIRef(_pred_prefix + 'prefLabel')
-        _altlabel_pred = URIRef(_pred_prefix + 'altLabel')
-        orgs = graph.subjects(_type_pred, _org_object)
-        for subject in orgs:
-            uri = subject.toPython()
-            parent = next(graph.objects(subject, _parent_pred), None)
-            if parent is not None:
-                parent = parent.toPython()
-            altLabel = next(graph.objects(subject, _altlabel_pred), None)
-            if altLabel is not None:
-                altLabel = altLabel.toPython()
-            else:
-                altLabel = ''
-            preflabel = next(graph.objects(subject, _preflabel_pred), None)
-            if preflabel is not None:
-                preflabel = preflabel.toPython()
-            else:
-                preflabel = ''
-            yield Institution(
-                uri=uri,
-                prefLabel=preflabel,
-                altLabel=altLabel,
-                #not present in TERN data
-                exactMatch='',
-                isUserAdded=False
-            )
+    # This is the old code for fetching the RDF. We've switched over the the SPARQL endpoint for now,
+    # but I don't want to delete this, just in case.
+    #http://linkeddata.tern.org.au/viewer/tern/id/http:/linkeddata.tern.org.au/def/Person?_view=skos&_format=application/rdf+xml
+    # @staticmethod
+    # def _fetch_tern_data(VocabName):
+    #     _pred_prefix = 'http://www.w3.org/2004/02/skos/core#'
+    #     _vocabServer = 'http://linkeddata.tern.org.au/viewer/tern/id/http:/linkeddata.tern.org.au/def/'
+    #     _query = '_view=skos&_format=application/rdf+xml'
+    #     url = '{base}{vocabName}?{query}'.format(base=_vocabServer,vocabName=VocabName,query=_query)
+    #     graph = Graph()
+    #     graph.parse(url, format='application/rdf+xml')
+    #     _type_pred = URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
+    #     _org_object = URIRef('http://schema.org/Organization')
+    #     _parent_pred = URIRef(_pred_prefix + 'broader')
+    #     _preflabel_pred = URIRef(_pred_prefix + 'prefLabel')
+    #     _altlabel_pred = URIRef(_pred_prefix + 'altLabel')
+    #     orgs = graph.subjects(_type_pred, _org_object)
+    #     for subject in orgs:
+    #         uri = subject.toPython()
+    #         parent = next(graph.objects(subject, _parent_pred), None)
+    #         if parent is not None:
+    #             parent = parent.toPython()
+    #         altLabel = next(graph.objects(subject, _altlabel_pred), None)
+    #         if altLabel is not None:
+    #             altLabel = altLabel.toPython()
+    #         else:
+    #             altLabel = ''
+    #         preflabel = next(graph.objects(subject, _preflabel_pred), None)
+    #         if preflabel is not None:
+    #             preflabel = preflabel.toPython()
+    #         else:
+    #             preflabel = ''
+    #         yield Institution(
+    #             uri=uri,
+    #             prefLabel=preflabel,
+    #             altLabel=altLabel,
+    #             #not present in TERN data
+    #             exactMatch='',
+    #             isUserAdded=False
+    #         )
--- backend/backend/management/commands/load_persons.py
+++ backend/backend/management/commands/load_persons.py
@@ -136,46 +136,46 @@ class Command(BaseCommand):
         return
 
     # This is the old code for fetching the RDF. We've switched over the the SPARQL endpoint for now,
-    # but I don't want to delete this, just in case.
-    @staticmethod
-    def _fetch_tern_data(VocabName):
-        _vocabServer = 'http://linkeddata.tern.org.au/viewer/tern/id/http:/linkeddata.tern.org.au/def/'
-        _query = '_view=skos&_format=application/rdf+xml'
-        url = '{base}{vocabName}?{query}'.format(base=_vocabServer,vocabName=VocabName,query=_query)
-        graph = Graph()
-        graph.parse(url, format='application/rdf+xml')
-        _type_pred = URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
-        _person_object = URIRef('http://schema.org/Person')
-        _familyName_pred = URIRef('http://schema.org/familyName')
-        _givenName_pred = URIRef('http://schema.org/givenName')
-        _honorificPrefix_pred = URIRef('http://schema.org/honorificPrefix')
-        _orgUri_pred = URIRef('http://schema.org/memberOf')
-        _orcid_pred = URIRef('http://schema.org/sameAs')
-        _email_pred = URIRef('http://schema.org/email')
-        _prefLabel_pred = URIRef('http://www.w3.org/2004/02/skos/core#prefLabel')
-        persons = graph.subjects(_type_pred, _person_object)
-        for subject in persons:
-            uri = subject.toPython()
-            familyName = Command.pred_value_or_empty(graph, subject, _familyName_pred)
-            givenName = Command.pred_value_or_empty(graph, subject, _givenName_pred)
-            honorificPrefix = Command.pred_value_or_empty(graph, subject, _honorificPrefix_pred)
-            orgUri = Command.pred_value_or_empty(graph, subject, _orgUri_pred)
-            prefLabel = Command.pred_value_or_empty(graph, subject, _prefLabel_pred)
-            email = Command.pred_value_or_empty(graph, subject, _email_pred)
-            orcid = Command.pred_value_or_empty(graph, subject, _orcid_pred)
-            orcid = orcid.replace('https://orcid.org/','')
-            orcid = orcid.replace('http://orcid.org/','')
-            # check that it's an orcid
-            if re.match(r"0000-000(1-[5-9]|2-[0-9]|3-[0-4])\d\d\d-\d\d\d[\dX]", orcid) is None:
-                orcid = ''
-            yield Person(
-                uri=uri,
-                orgUri=orgUri,
-                familyName=familyName,
-                givenName=givenName,
-                honorificPrefix=honorificPrefix,
-                orcid=orcid,
-                electronicMailAddress=email,
-                prefLabel=prefLabel,
-                isUserAdded=False
-            )
+    # but I don't want to delete this, just in case. 
+    # @staticmethod
+    # def _fetch_tern_data(VocabName):
+    #     _vocabServer = 'http://linkeddata.tern.org.au/viewer/tern/id/http:/linkeddata.tern.org.au/def/'
+    #     _query = '_view=skos&_format=application/rdf+xml'
+    #     url = '{base}{vocabName}?{query}'.format(base=_vocabServer,vocabName=VocabName,query=_query)
+    #     graph = Graph()
+    #     graph.parse(url, format='application/rdf+xml')
+    #     _type_pred = URIRef('http://www.w3.org/1999/02/22-rdf-syntax-ns#type')
+    #     _person_object = URIRef('http://schema.org/Person')
+    #     _familyName_pred = URIRef('http://schema.org/familyName')
+    #     _givenName_pred = URIRef('http://schema.org/givenName')
+    #     _honorificPrefix_pred = URIRef('http://schema.org/honorificPrefix')
+    #     _orgUri_pred = URIRef('http://schema.org/memberOf')
+    #     _orcid_pred = URIRef('http://schema.org/sameAs')
+    #     _email_pred = URIRef('http://schema.org/email')
+    #     _prefLabel_pred = URIRef('http://www.w3.org/2004/02/skos/core#prefLabel')
+    #     persons = graph.subjects(_type_pred, _person_object)
+    #     for subject in persons:
+    #         uri = subject.toPython()
+    #         familyName = Command.pred_value_or_empty(graph, subject, _familyName_pred)
+    #         givenName = Command.pred_value_or_empty(graph, subject, _givenName_pred)
+    #         honorificPrefix = Command.pred_value_or_empty(graph, subject, _honorificPrefix_pred)
+    #         orgUri = Command.pred_value_or_empty(graph, subject, _orgUri_pred)
+    #         prefLabel = Command.pred_value_or_empty(graph, subject, _prefLabel_pred)
+    #         email = Command.pred_value_or_empty(graph, subject, _email_pred)
+    #         orcid = Command.pred_value_or_empty(graph, subject, _orcid_pred)
+    #         orcid = orcid.replace('https://orcid.org/','')
+    #         orcid = orcid.replace('http://orcid.org/','')
+    #         # check that it's an orcid
+    #         if re.match(r"0000-000(1-[5-9]|2-[0-9]|3-[0-4])\d\d\d-\d\d\d[\dX]", orcid) is None:
+    #             orcid = ''
+    #         yield Person(
+    #             uri=uri,
+    #             orgUri=orgUri,
+    #             familyName=familyName,
+    #             givenName=givenName,
+    #             honorificPrefix=honorificPrefix,
+    #             orcid=orcid,
+    #             electronicMailAddress=email,
+    #             prefLabel=prefLabel,
+    #             isUserAdded=False
+    #         )

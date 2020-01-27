1300e9f6336320c59e5804efab61509a389d0f2b
Gerhard Weis <g.weis@uq.edu.au>
2020-01-28 09:00:25 +1000

    load initial data as part of migrations to avoid overwriting db changes

--- docker-files/docker_entrypoint.sh
+++ docker-files/docker_entrypoint.sh
@@ -3,7 +3,6 @@
 if [ "$1" == "init" ] ; then
   python3 manage.py migrate --noinput
   python3 manage.py setup_superuser --noinput
-  python3 manage.py loaddata setup.json
   python3 manage.py collectstatic --noinput
   # copy xml_templates to media folder if newer than existing files
   cp -u -v -r /data-submission-tool/xml_template/* /data/media/

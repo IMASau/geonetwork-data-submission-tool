c52b2b7d81157d7ea1f421ad9b6d6387ae30f4a6
Edmond Chuc <e.chuc@uq.edu.au>
2020-11-17 16:52:01 +1000

    Move elasticsearch import to module level

--- backend/frontend/config.py
+++ backend/frontend/config.py
@@ -1,10 +1,11 @@
 from django.apps import AppConfig
 from django.conf import settings
 
+from elasticsearch_dsl import connections
+
 
 class FrontendConfig(AppConfig):
     name = 'frontend'
 
     def ready(self):
-        from elasticsearch_dsl import connections
         connections.create_connection(hosts=[settings.ELASTICSEARCH_URL])

1d63ae82fb45609aabdba747172ff469f5240e17
Edmond Chuc <e.chuc@uq.edu.au>
2020-11-20 09:39:32 +1000

    Made Elasticsearch connection parameter verify_ssl configurable. Defaults to true.

--- backend/frontend/config.py
+++ backend/frontend/config.py
@@ -8,11 +8,18 @@ class FrontendConfig(AppConfig):
     name = 'frontend'
 
     def ready(self):
+        if hasattr(settings, 'ELASTICSEARCH_VERIFY_SSL'):
+            # Can be set to either true or false in the environment variable.
+            verify_ssl = settings.ELASTICSEARCH_VERIFY_SSL
+        else:
+            # If not set, default to true.
+            verify_ssl = True
+
         connections.create_connection(
             hosts=[settings.ELASTICSEARCH_URL],
             http_auth=(
                 settings.ELASTICSEARCH_USER,
                 settings.ELASTICSEARCH_PASSWORD
             ),
-            verify_ssl=False
+            verify_ssl=verify_ssl
         )

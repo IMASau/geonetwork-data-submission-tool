5a8108c08c998ae69ac85e2ff61882abcc7cdd8b
Mark Hepburn <Mark.Hepburn@gmail.com>
2021-05-13 13:57:48 +1000

    Template context-processors fix-up

--- MetadataServer/webapp/settings.py
+++ MetadataServer/webapp/settings.py
@@ -106,14 +106,13 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': (
                 "django.contrib.auth.context_processors.auth",
-                "django.core.context_processors.debug",
-                "django.core.context_processors.i18n",
-                "django.core.context_processors.media",
-                "django.core.context_processors.request",
-                "django.core.context_processors.static",
-                "django.core.context_processors.tz",
+                "django.template.context_processors.debug",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.request",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
                 "django.contrib.messages.context_processors.messages",
-                "allauth.account.context_processors.account",
             ),
             'loaders': (
                 'django.template.loaders.filesystem.Loader',

66fd5f6fe427ee9eac45e983f753ebb4f7689208
olivergeorge <oliver@condense.com.au>
2021-06-01 12:30:36 +1000

    Put elastic search feature behind a flag
    A quick fix.  The flag disables init_elasticsearch in FrontendConfig
    
    I suspect a better approach is a separate app for TERN APIs - they are fully independent and that gives some clean separation

--- backend/frontend/config.py
+++ backend/frontend/config.py
@@ -1,19 +1,21 @@
 from django.apps import AppConfig
 from django.conf import settings
 
-from elasticsearch_dsl import connections
+if settings.USE_TERN_ELASTICSEARCH:
+    from elasticsearch_dsl import connections
 
 
-def init_elasticsearch():
-    connections.create_connection(
-        hosts=[settings.ELASTICSEARCH_URL],
-        http_auth=(settings.ELASTICSEARCH_USER, settings.ELASTICSEARCH_PASSWORD),
-        verify_certs=settings.ELASTICSEARCH_VERIFY_SSL,
-    )
+    def init_elasticsearch():
+        connections.create_connection(
+            hosts=[settings.ELASTICSEARCH_URL],
+            http_auth=(settings.ELASTICSEARCH_USER, settings.ELASTICSEARCH_PASSWORD),
+            verify_certs=settings.ELASTICSEARCH_VERIFY_SSL,
+        )
 
 
 class FrontendConfig(AppConfig):
     name = "frontend"
 
     def ready(self):
-        init_elasticsearch()
+        if settings.USE_TERN_ELASTICSEARCH:
+            init_elasticsearch()

f181a2e36824034967fca92aae58f0576f1fa193
Oliver George <oliver@condense.com.au>
2021-08-12 20:44:51 +1000

    add shadow-cljs style interop.react-virtualized

---
+++ frontend/src/io2/interop/react_virtualized.cljs
@@ -0,0 +1,9 @@
+(ns interop.react-virtualized
+  (:require ["react" :default React]
+            ["react-window" :as ReactWindow]
+            ["react-virtualized" :as ReactVirtualized]
+            ["react-virtualized-auto-sizer" :default AutoSizer]))
+
+(def ReactWindow* (React/createFactory ReactWindow/FixedSizeList))
+(def AutoSizer* (React/createFactory ReactVirtualized/AutoSizer))
+(def VirtualScroll* (React/createFactory ReactVirtualized/VirtualScroll))

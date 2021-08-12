3c172d68079f62d313b9390f7358effc91e8505d
Oliver George <oliver@condense.com.au>
2021-08-12 19:16:39 +1000

    Redundant let expression

--- frontend/src/cljs/metcalf3/views.cljs
+++ frontend/src/cljs/metcalf3/views.cljs
@@ -1584,38 +1584,38 @@
   (letfn [(init-state [_]
             {:reset-file-drop (chan)})
           (render [this]
-            (let [{:keys [file reset-file-drop uploading]} (r/state this)]
-              (let [attachments-path [:form :fields :attachments]
-                    {:keys [disabled] :as attachments} @(rf/subscribe [:subs/get-derived-path attachments-path])
-                    upload-form @(rf/subscribe [:subs/get-derived-path [:upload_form]])]
-                [:div.UploadData {:class (if disabled "disabled")}
-                 (if-not (empty? (:value attachments))
-                   [:div
-                    [:table.table.table-hover
-                     [:thead
-                      [:tr [:th "Name"]]]
-                     [:tbody
-                      (for [[attachment-idx attachment] (map-indexed vector (:value attachments))]
-                        (let [{:keys [file name]} (:value attachment)]
-                          [:tr
-                           [:td
-                            [:a {:href (:value file) :target "blank"} (:value name)]
-                            [:button.btn.btn-warn.btn-xs.pull-right
-                             {:on-click #(delete-attachment! attachments-path attachment-idx)
-                              :disabled disabled}
-                             [:span.glyphicon.glyphicon-minus]]]]))]]]
-                   [:p "There are no data files attached to this record"])
-                 (when-not disabled
-                   [:div
-                    [FileDrop
-                     {:name         "file"
-                      :max-filesize 100
-                      :reset-ch     reset-file-drop
-                      :on-change    #(r/set-state this {:file %})}]
-                    [:button.btn.btn-primary
-                     {:on-click #(upload! this upload-form file reset-file-drop)
-                      :disabled (or uploading (not file))}
-                     "Confirm Upload"]])])))]
+            (let [{:keys [file reset-file-drop uploading]} (r/state this)
+                  attachments-path [:form :fields :attachments]
+                  {:keys [disabled] :as attachments} @(rf/subscribe [:subs/get-derived-path attachments-path])
+                  upload-form @(rf/subscribe [:subs/get-derived-path [:upload_form]])]
+              [:div.UploadData {:class (if disabled "disabled")}
+               (if-not (empty? (:value attachments))
+                 [:div
+                  [:table.table.table-hover
+                   [:thead
+                    [:tr [:th "Name"]]]
+                   [:tbody
+                    (for [[attachment-idx attachment] (map-indexed vector (:value attachments))]
+                      (let [{:keys [file name]} (:value attachment)]
+                        [:tr
+                         [:td
+                          [:a {:href (:value file) :target "blank"} (:value name)]
+                          [:button.btn.btn-warn.btn-xs.pull-right
+                           {:on-click #(delete-attachment! attachments-path attachment-idx)
+                            :disabled disabled}
+                           [:span.glyphicon.glyphicon-minus]]]]))]]]
+                 [:p "There are no data files attached to this record"])
+               (when-not disabled
+                 [:div
+                  [FileDrop
+                   {:name         "file"
+                    :max-filesize 100
+                    :reset-ch     reset-file-drop
+                    :on-change    #(r/set-state this {:file %})}]
+                  [:button.btn.btn-primary
+                   {:on-click #(upload! this upload-form file reset-file-drop)
+                    :disabled (or uploading (not file))}
+                   "Confirm Upload"]])]))]
     (r/create-class
       {:get-initial-state init-state
        :render            render})))

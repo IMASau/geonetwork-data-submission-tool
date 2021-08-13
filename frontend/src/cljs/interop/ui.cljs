3e9109f7474f79e82e83c2960f20890208a6ae34
Oliver George <oliver@condense.com.au>
2021-08-13 14:37:48 +1000

    WIP on ui components

---
+++ frontend/src/cljs/interop/ui.cljs
@@ -0,0 +1,4 @@
+(ns interop.ui
+  (:require ["/ui/components/Button" :as Button]))
+
+(def Button Button/Button)
--- frontend/src/js/ui/components/Button.stories.jsx
+++ frontend/src/js/ui/components/Button.stories.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { Button } from './Button';
+import './button.css';
 
 export default {
   title: 'Example/Button',

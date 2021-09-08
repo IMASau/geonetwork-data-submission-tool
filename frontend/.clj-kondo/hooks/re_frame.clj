b8916416aaf1c52cab3d7a0282eb8b15a2e17900
Oliver George <oliver@condense.com.au>
2021-09-08 15:04:22 +1000

    setup for linting
    includes hooks to check re-frame keyword use

--- frontend/.clj-kondo/hooks/re_frame.clj
+++ frontend/.clj-kondo/hooks/re_frame.clj
@@ -1,8 +1,8 @@
 (ns hooks.re-frame
   (:require [clj-kondo.hooks-api :as api]))
 
-
-(defn dispatch [{:keys [:node]}]
+(defn use-qualified-keyword
+  [{:keys [:node]}]
   (let [sexpr (api/sexpr node)
         event (second sexpr)
         kw (first event)]
@@ -15,16 +15,15 @@
                            :row     row
                            :col     col})))))
 
-(defn subscribe [{:keys [:node]}]
-  (let [sexpr (api/sexpr node)
-        event (second sexpr)
-        kw (first event)]
-    (when (and (vector? event)
-               (keyword? kw)
+(defn reg-qualified-keyword
+  [{:keys [:node]}]
+  (let [c (:children node)
+        sexpr (api/sexpr node)
+        kw (second sexpr)
+        {:keys [:row :col]} (some-> node :children second meta)]
+    (when (and (keyword? kw)
                (not (qualified-keyword? kw)))
-      (let [{:keys [:row :col]} (some-> node :children second :children first meta)]
-        (api/reg-finding! {:message "keyword should be fully qualified!"
-                           :type    :re-frame/keyword
-                           :row     row
-                           :col     col})))))
-
+      (api/reg-finding! {:message "keyword should be fully qualified!"
+                         :type    :re-frame/keyword
+                         :row     row
+                         :col     col}))))

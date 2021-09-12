e48f75cd6deaff60bfe5487b82607067477c6d23
Oliver George <oliver@condense.com.au>
2021-09-13 09:12:22 +1000

    WIP on TextareaField

---
+++ frontend/src/js/ui/components/TextareaField/TextareaField.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import * as BPCore from '@blueprintjs/core';
+import {hasErrorIntent} from "../utils";
+
+
+export function TextareaField({value, hasError, disabled, placeholder, onChange}) {
+    const intent = hasErrorIntent({hasError, disabled});
+    return (
+        <div className="TextareaField">
+            <BPCore.TextArea
+                growVertically={true}
+                intent={intent}
+                disabled={disabled}
+                value={value}
+                placeholder={placeholder}
+                onChange={(e) => onChange(e.target.value)}
+            />
+        </div>
+    );
+}
+
+TextareaField.propTypes = {
+    value: PropTypes.string,
+    placeholder: PropTypes.string,
+    disabled: PropTypes.bool,
+    hasError: PropTypes.bool,
+    onChange: PropTypes.func,
+}

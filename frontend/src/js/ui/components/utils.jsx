4ffc6f3a169b0025f7f245c9f842beffec3f4a9b
Oliver George <oliver@condense.com.au>
2021-09-18 13:48:59 +1000

    Add utils.useCachedState
    Helper for inputs which hold value in state and report change on blur

--- frontend/src/js/ui/components/utils.jsx
+++ frontend/src/js/ui/components/utils.jsx
@@ -1,6 +1,15 @@
+import React from 'react';
 import * as BPCore from "@blueprintjs/core";
 import {Button, Icon} from "@blueprintjs/core";
 import {Tooltip2} from "@blueprintjs/popover2";
+
+export function useCachedState(value) {
+    const [stateValue, setStateValue] = React.useState(value);
+    React.useEffect(() => {
+        setStateValue(value)
+    }, [value])
+    return [stateValue, setStateValue]
+}
 
 export function hasErrorIntent({hasError, disabled}) {
     return (hasError && !disabled) ? BPCore.Intent.DANGER : BPCore.Intent.NONE;

598a556a45b58cd8f8206e005ecd16af29fb0051
olivergeorge <oliver@condense.com.au>
2021-09-23 20:58:08 +1000

    Add metcalf3.rules/validate-required-field-rule

--- frontend/src/cljs/metcalf3/rules.cljs
+++ frontend/src/cljs/metcalf3/rules.cljs
@@ -4,3 +4,12 @@
 (defn get-rule-handler [{:keys [ruleId]}] (get rule-registry ruleId identity))
 (defn apply-rule [field rule] ((get-rule-handler rule) field rule))
 (defn apply-rules [field] (reduce apply-rule field (:rules field)))
+
+(defn validate-required-field-rule
+  [field rule]
+  (let [{:keys [value]} field
+        empty? (contains? #{nil "" [] {} #{}} value)]
+    (-> field
+        (assoc :required true)
+        (cond-> empty?
+                (update field :errors "This field is required")))))

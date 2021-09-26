87063522f791ed4801f4b43c25d77acc225afc7b
olivergeorge <oliver@condense.com.au>
2021-09-27 09:58:03 +1000

    simplify metcalf3.preload
    no need for debug test

--- frontend/src/cljs/metcalf3/preload.cljs
+++ frontend/src/cljs/metcalf3/preload.cljs
@@ -1,6 +1,5 @@
 (ns metcalf3.preload
   (:require [cljs.spec.alpha :as s]))
 
-(when ^boolean js/goog.DEBUG
-  (enable-console-print!)
-  (s/check-asserts true))
+(enable-console-print!)
+(s/check-asserts true)

2322dc5d49e2c8d637e1909e84a4595a09189d68
olivergeorge <oliver@condense.com.au>
2021-09-27 09:57:27 +1000

    preload devtools via compile config

--- frontend/src/cljs/metcalf3/preload.cljs
+++ frontend/src/cljs/metcalf3/preload.cljs
@@ -1,8 +1,6 @@
 (ns metcalf3.preload
-  (:require [cljs.spec.alpha :as s]
-            [devtools.core :as devtools]))
+  (:require [cljs.spec.alpha :as s]))
 
 (when ^boolean js/goog.DEBUG
   (enable-console-print!)
-  (devtools/install!)
   (s/check-asserts true))

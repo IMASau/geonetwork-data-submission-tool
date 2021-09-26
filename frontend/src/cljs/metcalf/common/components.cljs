e47ffad87f22c532b579bfc388d4c363df76bae3
olivergeorge <oliver@condense.com.au>
2021-09-26 16:38:11 +1000

    Add metcalf.common.components

---
+++ frontend/src/cljs/metcalf/common/components.cljs
@@ -0,0 +1,78 @@
+(ns metcalf.common.components
+  (:require [clojure.string :as string]
+            [interop.date :as date]
+            [interop.ui :as ui]
+            [re-frame.core :as rf]))
+
+
+(defn input-field-with-label
+  [{:keys [path] :as config}]
+  (let [logic @(rf/subscribe [::get-input-field-with-label-props path])
+        props (merge (select-keys config [:label :placeholder :helperText :toolTip]) logic)
+        {:keys [label placeholder helperText toolTip maxLength required value disabled show-errors errors]} props
+        onChange #(rf/dispatch [::input-field-with-label-value-changed path %])
+        hasError (when (and show-errors (seq errors)) true)]
+    [ui/FormGroup
+     {:label      label
+      :required   required
+      :disabled   disabled
+      :hasError   hasError
+      :helperText (if hasError (string/join ". " errors) helperText)
+      :toolTip    toolTip}
+     [ui/InputField
+      {:value       (or value "")                           ; TODO: should be guaranteed by sub
+       :placeholder placeholder
+       :maxLength   maxLength
+       :disabled    disabled
+       :hasError    hasError
+       :onChange    onChange}]]))
+
+
+(defn textarea-field-with-label
+  [{:keys [path] :as config}]
+  (let [config-keys [:label :placeholder :helperText :toolTip :rows]
+        logic @(rf/subscribe [::get-textarea-field-with-label-props path])
+        onChange #(rf/dispatch [::textarea-field-with-label-value-changed path %])
+        props (merge (select-keys config config-keys) logic)
+        {:keys [label placeholder helperText toolTip rows
+                maxLength required value disabled show-errors errors]} props
+        hasError (when (and show-errors (seq errors)) true)]
+    [ui/FormGroup
+     {:label      label
+      :required   required
+      :disabled   disabled
+      :hasError   hasError
+      :helperText (if hasError (string/join ". " errors) helperText)
+      :toolTip    toolTip}
+     [ui/TextareaField
+      {:value       (or value "")                           ; TODO: should be guaranteed by sub
+       :placeholder placeholder
+       :disabled    disabled
+       :hasError    hasError
+       :maxLength   maxLength
+       :rows        rows
+       :onChange    onChange}]]))
+
+
+(defn date-field-with-label
+  [{:keys [path] :as config}]
+  (let [config-keys [:label :required :helperText :toolTip :minDate :maxDate]
+        logic @(rf/subscribe [::get-date-field-with-label-props path])
+        props (merge (select-keys config config-keys) logic)
+        {:keys [label required helperText toolTip minDate maxDate
+                value disabled errors show-errors]} props
+        hasError (when (and show-errors (seq errors)) true)]
+    [ui/FormGroup
+     {:label      label
+      :required   required
+      :disabled   disabled
+      :hasError   hasError
+      :helperText (if hasError (string/join ". " errors) helperText)
+      :toolTip    toolTip}
+     [ui/DateField
+      {:value    (date/from-value value)
+       :disabled disabled
+       :onChange #(rf/dispatch [::date-field-with-label-value-change path (date/to-value %)])
+       :hasError hasError
+       :minDate  (date/from-value minDate)
+       :maxDate  (date/from-value maxDate)}]]))

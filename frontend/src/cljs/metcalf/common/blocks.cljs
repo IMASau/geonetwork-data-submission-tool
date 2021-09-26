93aa06603e8cd0f6c9de7206545330e4c8a8ecc4
olivergeorge <oliver@condense.com.au>
2021-09-26 16:37:18 +1000

    Add metcalf.common.blocks

---
+++ frontend/src/cljs/metcalf/common/blocks.cljs
@@ -0,0 +1,60 @@
+(ns metcalf.common.blocks
+  (:require [cljs.spec.alpha :as s]
+            [metcalf.common.schema :as schema]))
+
+
+(s/def ::type string?)
+(s/def ::properties map?)
+(s/def ::content
+  (s/or :arr (s/coll-of ::block)
+        :obj (s/map-of string? ::block)))
+(s/def ::block
+  (s/keys :opt-un [::type ::properties ::content]))
+
+
+(defn update-vals [m f] (zipmap (keys m) (map f (vals m))))
+
+
+(defn walk
+  [inner outer block]
+  (case (:type block)
+    "array" (update block :content #(mapv inner %))
+    "object" (outer (update block :content update-vals inner))
+    (outer block)))
+
+
+(defn prewalk [f form]
+  (walk (partial prewalk f) identity (f form)))
+
+
+(defn postwalk [f form]
+  (walk (partial postwalk f) f form))
+
+
+(defn as-blocks
+  "Return blocks given data and a schema."
+  [{:keys [data schema]}]
+  (schema/postwalk-schema-data
+    (fn [{:keys [data schema]}]
+      (let [type (:type schema)]
+        (merge {:type type}
+               (case type
+                 "array" {:content data}
+                 "object" {:content data}
+                 {:properties {:value data}})
+               (when-let [rules (:rules schema)]
+                 {:rules rules}))))
+    {:data data :schema schema}))
+
+
+(defn as-data
+  "Return data given blocks"
+  [{:keys [type properties content]}]
+  (case type
+    "array" (mapv as-data content)
+    "object" (zipmap (keys content) (map as-data (vals content)))
+    (:value properties)))
+
+
+(defn block-path [data-path]
+  (vec (interleave (repeat :content) data-path)))

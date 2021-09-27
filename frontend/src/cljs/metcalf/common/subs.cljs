2df8f932c0fa588e688e3c5ea0061f76360fbbc2
olivergeorge <oliver@condense.com.au>
2021-09-27 10:43:28 +1000

    fix component subs

--- frontend/src/cljs/metcalf/common/subs.cljs
+++ frontend/src/cljs/metcalf/common/subs.cljs
@@ -3,15 +3,15 @@
 
 (defn get-input-field-with-label-props
   [db [_ db-path]]
-  (get-in db db-path))
+  (get-in db (conj db-path :properties)))
 
 
 (defn get-textarea-field-with-label-props
   [db [_ db-path]]
-  (get-in db db-path))
+  (get-in db (conj db-path :properties)))
 
 
 ; FIXME: leaking empty strings instead of null from payload.forms.data
 (defn get-date-field-with-label-props
   [db [_ db-path]]
-  (get-in db db-path))
+  (get-in db (conj db-path :properties)))

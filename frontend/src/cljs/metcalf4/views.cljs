f17a60840903ff1b02b43182fb581235cd4746ad
olivergeorge <oliver@condense.com.au>
2021-09-30 09:03:57 +1000

    Port pure fns from views3

--- frontend/src/cljs/metcalf4/views.cljs
+++ frontend/src/cljs/metcalf4/views.cljs
@@ -1,6 +1,9 @@
 (ns metcalf4.views
-  (:require [clojure.string :as string]))
-
+  (:require [cljs.spec.alpha :as s]
+            [clojure.string :as string]
+            [goog.object :as gobj]
+            [interop.react-imask :as react-imask]
+            [reagent.core :as r]))
 
 ; For pure views only, no re-frame subs/handlers
 
@@ -22,122 +25,130 @@
   (when (and show-errors (seq errors))
     "has-error"))
 
-#_metcalf3.views/dp-term-paths
-#_metcalf3.views/masked-text-widget
-#_metcalf3.views/InputWidget
-#_metcalf3.views/filter-name
-#_metcalf3.views/NameInputWidget
-#_metcalf3.views/SimpleInputWidget
-#_metcalf3.views/filter-table
-#_metcalf3.views/InputField
-#_metcalf3.views/input-field-with-label
-#_metcalf3.views/date-field-with-label
-#_metcalf3.views/date-field
-#_metcalf3.views/OptionWidget
-#_metcalf3.views/SelectWidget
-#_metcalf3.views/SelectField
-#_metcalf3.views/textarea-widget
-#_metcalf3.views/textarea-field
-#_metcalf3.views/textarea-field-with-label
-#_metcalf3.views/Checkbox
-#_metcalf3.views/CheckboxField
-#_metcalf3.views/BackButton
-#_metcalf3.views/getter
-#_metcalf3.views/update-table-width
-#_metcalf3.views/KeywordsThemeCell
-#_metcalf3.views/TopicCategoryCell
-#_metcalf3.views/KeywordsThemeTable
-#_metcalf3.views/handle-highlight-new
-#_metcalf3.views/modal-dialog-table-modal-edit-form
-#_metcalf3.views/modal-dialog-table-modal-add-form
-#_metcalf3.views/TableModalEdit
-#_metcalf3.views/theme-option-renderer
-#_metcalf3.views/modal-dialog-theme-keywords
-#_metcalf3.views/TopicCategories
-#_metcalf3.views/ThemeKeywords
-#_metcalf3.views/ThemeInputField
-#_metcalf3.views/ThemeKeywordsExtra
-#_metcalf3.views/TaxonKeywordsExtra
-#_metcalf3.views/->float
-#_metcalf3.views/CoordInputWidget
-#_metcalf3.views/CoordInputField
-#_metcalf3.views/CoordField
-#_metcalf3.views/GeographicCoverage
-#_metcalf3.views/breadcrumb-renderer
-#_metcalf3.views/nasa-list-renderer
-#_metcalf3.views/other-term?
-#_metcalf3.views/NasaListSelectField
-#_metcalf3.views/Tooltip
-#_metcalf3.views/ElasticsearchSelectField
-#_metcalf3.views/PersonListWidget
-#_metcalf3.views/ApiTreeWidget
-#_metcalf3.views/ApiTermTreeField
-#_metcalf3.views/TermOrOtherForm
-#_metcalf3.views/UnitTermOrOtherForm
-#_metcalf3.views/PersonListField
-#_metcalf3.views/PersonForm
-#_metcalf3.views/modal-dialog-parametername
-#_metcalf3.views/modal-dialog-parameterunit
-#_metcalf3.views/modal-dialog-parameterinstrument
-#_metcalf3.views/modal-dialog-parameterplatform
-#_metcalf3.views/modal-dialog-person
-#_metcalf3.views/DataParameterRowEdit
-#_metcalf3.views/DataParametersTable
-#_metcalf3.views/upload!
-#_metcalf3.views/handle-file
-#_metcalf3.views/FileDrop
-#_metcalf3.views/delete-attachment!
-#_metcalf3.views/UploadData
-#_metcalf3.views/save!
-#_metcalf3.views/handle-submit-click
-#_metcalf3.views/IMASLodge
-#_metcalf3.views/Lodge
-#_metcalf3.views/AddressField
-#_metcalf3.views/OrganisationPickerWidget
-#_metcalf3.views/PersonPickerWidget
-#_metcalf3.views/SelectRoleWidget
-#_metcalf3.views/PersonInputField
-#_metcalf3.views/OrganisationInputField
-#_metcalf3.views/ResponsiblePartyField
-#_metcalf3.views/FieldError
-#_metcalf3.views/ManyFieldError
-#_metcalf3.views/PageErrors
-#_metcalf3.views/help
-#_metcalf3.views/help
-#_metcalf3.views/navbar
-#_metcalf3.views/PageView404
-#_metcalf3.views/PageViewError
-#_metcalf3.views/CreditField
-#_metcalf3.views/delete-contact!
-#_metcalf3.views/parties-list
-#_metcalf3.views/default-selected-group
-#_metcalf3.views/Who
-#_metcalf3.views/MethodOrOtherForm
-#_metcalf3.views/Methods
-#_metcalf3.views/UseLimitationsFieldEdit
-#_metcalf3.views/UseLimitations
-#_metcalf3.views/SupplementalInformationRowEdit
-#_metcalf3.views/IMASSupplementalInformation
-#_metcalf3.views/SupplementalInformation
-#_metcalf3.views/ResourceConstraints
-#_metcalf3.views/SupportingResourceFieldEdit
-#_metcalf3.views/IMASSupportingResource
-#_metcalf3.views/SupportingResource
-#_metcalf3.views/DataSourceRowEdit
-#_metcalf3.views/DataSources
-#_metcalf3.views/progress-bar
-#_metcalf3.views/handle-archive-click
-#_metcalf3.views/edit-tabs
-#_metcalf3.views/PageViewEdit
-#_metcalf3.views/FormErrors
-#_metcalf3.views/NewDocumentForm
-#_metcalf3.views/modal-dialog-dashboard-create-modal
-#_metcalf3.views/NewDocumentButton
-#_metcalf3.views/clone-doc
-#_metcalf3.views/DocumentTeaser
-#_metcalf3.views/PageViewDashboard
-#_metcalf3.views/LegacyIECompatibility
-#_metcalf3.views/modal-dialog-alert
-#_metcalf3.views/modal-dialog-confirm
-#_metcalf3.views/ModalStack
-#_metcalf3.views/AppRoot
+(defn masked-text-widget
+  [{:keys [mask value placeholder disabled on-change on-blur]}]
+  [react-imask/masked-input
+   {:mask        mask
+    :disabled    disabled
+    :value       value
+    :class       "form-control"
+    :placeholder placeholder
+    :on-change   on-change
+    :on-blur     on-blur}])
+
+(defn filter-name [s]
+  (s/assert string? s)
+  (string/replace s #"[^a-zA-Z-', ]" ""))
+
+(defn filter-table
+  "Default search for local datasource: case-insensitive substring match"
+  [simple? table query]
+  (s/assert string? query)
+  (let [col-match? (if simple?
+                     #(string/starts-with? (-> % str string/lower-case) (string/lower-case query))
+                     #(string/includes? (-> % str string/lower-case) (string/lower-case query)))]
+    (filter
+      (fn [row]
+        (some col-match? (rest row)))
+      table)))
+
+(defn OptionWidget [props]
+  (let [[value display] props]
+    [:option {:value value} display]))
+
+(defn Checkbox [props]
+  (let [{:keys [label checked on-change disabled help]
+         :or   {checked false}} props
+        input-control [:input (merge {:type     "checkbox"
+                                      :checked  (boolean checked)
+                                      :disabled disabled
+                                      :onChange on-change})]]
+    [:div.form-group {:class (validation-state props)}
+     [:div.checkbox
+      [:label input-control label]]
+     [:p.help-block help]]))
+
+(defn KeywordsThemeCell [rowData]
+  (let [rowData (take-while (complement empty?) rowData)]
+    [:div.topic-cell
+     [:div.topic-path (string/join " > " (drop-last (rest rowData)))]
+     [:div.topic-value (last rowData)]]))
+
+(defn TopicCategoryCell [rowData]
+  (let [rowData (take-while (complement empty?) rowData)]
+    [:div.topic-cell
+     [:div.topic-value (last rowData)]]))
+
+(defn handle-highlight-new [this item]
+  (r/set-state this {:highlight (conj (:highlight (r/state this)) item)})
+  (js/setTimeout #(r/set-state this {:highlight (disj (:highlight (r/state this)) item)}) 5000))
+
+(defn theme-option-renderer
+  [props]
+  (let [rowData (gobj/get props "rowData")]
+    [:div
+     [KeywordsThemeCell rowData]]))
+
+(defn ->float [s]
+  (let [f (js/parseFloat s)]
+    (if (js/isNaN f) nil f)))
+
+(defn breadcrumb-renderer [selected-option]
+  (let [text (gobj/get selected-option "breadcrumb")
+        term-text (gobj/get selected-option "term")
+        alt-label (gobj/get selected-option "altLabel")]
+    [:div.topic-cell {:key term-text}
+     [:div.topic-path text]
+     [:div.topic-value term-text]
+     [:div {:style
+            {:margin-left 10 :color "#929292" :font-size 11}}
+      (if (clojure.string/blank? alt-label) "" (concat "also known as " alt-label))]]))
+
+(defn nasa-list-renderer [option]
+  (aget option "prefLabel"))
+
+(defn other-term?
+  [term vocabularyTermURL]
+  (and (:value term) (empty? (:value vocabularyTermURL))))
+
+(defn Tooltip
+  [value]
+  [:span " "
+   [:i.icon-info-sign.tern-tooltip
+    [:span.tern-tooltiptext value]]])
+
+(defn FieldError [{:keys [errors label]}]
+  [:span.FieldError label ": " (first errors)])
+
+(defn ManyFieldError [{:keys [errors label]}]
+  [:span.FieldError label ": " (or (first errors) "check field errors")])
+
+(defn ResourceConstraints []
+  [:div.ResourceConstraints
+   [:p.help-block (str "Creative Commons - Attribution 4.0 International. The license allows others to copy,
+   distribute, display, and create derivative works provided that they
+   credit the original source and any other nominated parties.")]
+   [:p [:a {:href   "https://creativecommons.org/licenses/by/4.0/"
+            :target "_blank"}
+        "https://creativecommons.org/licenses/by/4.0/"]]])
+
+(defn LegacyIECompatibility
+  [_ _]
+  [:div.LegacyIECompatibility
+   [:div.row
+    [:div.col-md-6.col-md-offset-3
+     [:div.container.box
+      [:h1 "Browser not supported"]
+      [:p.lead "The work request system doesn't support early versions of Internet Explorer."]
+      [:p.lead "Please use Google Chrome to access this system or upgrade your browser."]
+      [:hr]
+      [:p "Related links:"]
+      [:ul
+       [:li [:a {:href "http://www.techtimes.com/articles/12659/20140811/dingdong-internet-explorer-8-is-dead-microsoft-will-end-its-life-in-january-2016.htm"}
+             "Dingdong, Internet Explorer 8 is dead: Microsoft will end its life in January 2016 (TechTimes)"]
+        " and IE 9 will follow a year later."]
+       [:li [:a {:href "http://www.computerworld.com/article/2492571/web-apps/google-to-drop-support-for-ie8-on-nov--15.html"}
+             "Google to drop support for IE8 on Nov 15 [2012]"]]
+       [:li [:a {:href "http://www.w3counter.com/globalstats.php"}
+             "Market share of IE8 and IE9 is around 2% each world wide."]]]
+      [:br]]]]])

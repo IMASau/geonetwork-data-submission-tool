4adf8d605ff684de22599d8fe4d30f2bfa6874da
Oliver George <oliver@condense.com.au>
2021-10-01 11:03:03 +1000

    add components4/form-group

--- frontend/src/cljs/metcalf4/components.cljs
+++ frontend/src/cljs/metcalf4/components.cljs
@@ -41,6 +41,23 @@
             (into [:ul] (for [msg msgs] [:li msg]))]
            (first msgs))]))))
 
+(defn form-group
+  [config & children]
+  (let [ctx (utils4/get-ctx config)
+        config-keys [:label :placeholder :helperText :toolTip]
+        logic @(rf/subscribe [::get-block-props ctx])
+        props (merge logic (select-keys config config-keys))
+        {:keys [label helperText toolTip required disabled show-errors errors]} props
+        hasError (when (and show-errors (seq errors)) true)]
+    (into [ui/FormGroup
+           {:label      label
+            :required   required
+            :disabled   disabled
+            :hasError   hasError
+            :helperText (if hasError (string/join ". " errors) helperText)
+            :toolTip    toolTip}]
+          children)))
+
 (defn input-field-with-label
   [config]
   (let [ctx (utils4/get-ctx config)

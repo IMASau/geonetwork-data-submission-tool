0e4b972330298f430f232eac4781f631772fa43b
Oliver George <oliver@condense.com.au>
2021-10-01 16:24:50 +1000

    Add vec-insert

--- frontend/src/cljs/metcalf3/utils.cljs
+++ frontend/src/cljs/metcalf3/utils.cljs
@@ -93,6 +93,10 @@
   {:pre [(vector? v) (nat-int? i) (contains? v i)]}
   (reduce conj (vec (subvec v 0 i)) (subvec v (inc i) (count v))))
 
+(defn vec-insert [v idx x]
+  {:pre [(vector? v) (nat-int? idx)]}
+  (into (conj (subvec v 0 idx) x) (subvec v idx)))
+
 (defn zip [& colls]
   (apply map vector colls))
 

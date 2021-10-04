8c648fdf5ef811b53af4ca6255341792f21ecee9
olivergeorge <oliver@condense.com.au>
2021-10-04 22:56:12 +1100

    Assert that schema matches config

--- frontend/src/cljs/metcalf4/components.cljs
+++ frontend/src/cljs/metcalf4/components.cljs
@@ -487,6 +487,11 @@
         props (merge logic (select-keys config config-keys))
         {:keys [placeholder uri disabled errors show-errors]} props
         hasError (when (and show-errors (seq errors)) true)]
+
+    (schema/assert-compatible-schema
+      {:schema1 @(rf/subscribe [::get-data-schema ctx])
+       :schema2 {:type "array" {:type "object" :properties {}}}})
+
     [ui/AsyncSelectOptionField
      {:value       nil
       :loadOptions #(utils4/fetch-post {:uri uri :body {:query %}})

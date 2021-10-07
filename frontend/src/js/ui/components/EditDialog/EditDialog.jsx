48218ca1b78ef737be225594463e96d8fba82cae
olivergeorge <oliver@condense.com.au>
2021-10-08 08:59:53 +1100

    WIP EditDialog

---
+++ frontend/src/js/ui/components/EditDialog/EditDialog.jsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import {Button, Classes, Dialog, Intent} from "@blueprintjs/core";
+
+EditDialog.propTypes = {
+    title: PropTypes.string.isRequired,
+    body: PropTypes.elementType.isRequired,
+    onClear: PropTypes.func.isRequired,
+    onSave: PropTypes.func.isRequired,
+    canSave: PropTypes.bool,
+}
+
+export function EditDialog({title, body, onClear, onSave, canSave}) {
+    const [isOpen, setOpen] = React.useState(false);
+    function open() {setOpen(true)}
+    function close() {setOpen(false)}
+    function clear() {close(); onClear()}
+    function save() {close(); onSave()}
+    return (
+        <div>
+            <Button onClick={open}>Show dialog</Button>
+            <Dialog
+                icon="info-sign"
+                onClose={close}
+                title={title}
+                canEscapeKeyClose={false}
+                canOutsideClickClose={false}
+                isOpen={isOpen}
+                usePortal={true}
+            >
+                <div className={Classes.DIALOG_BODY}>
+                    {body}
+                </div>
+                <div className={Classes.DIALOG_FOOTER}>
+                    <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+                        <Button onClick={clear}>Clear</Button>
+                        <Button onClick={save} disabled={canSave} intent={Intent.PRIMARY}>Save</Button>
+                    </div>
+                </div>
+            </Dialog>
+        </div>
+    )
+}
---
+++ frontend/src/js/ui/components/EditDialog/EditDialog.stories.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import {EditDialog} from './EditDialog';
+import '@blueprintjs/core/lib/css/blueprint.css';
+
+export default {
+    title: 'Example/EditDialog',
+    component: EditDialog,
+    argTypes: {
+        onOpen: {action: 'onOpen'},
+        onClose: {action: 'onClose'},
+    }
+};
+
+const FieldTemplate = (args) => <EditDialog {...args} />;
+
+export const SimpleField = FieldTemplate.bind({});
+SimpleField.args = {
+    body: <div>Do you want to build a snowman?</div>
+};
+
+export const DoubleDialog = FieldTemplate.bind({});
+DoubleDialog.args = {
+    title: "First dialog",
+    body: <EditDialog title="Second dialog"
+                      body="Another dialog!"
+                      onClear={()=>{}}
+                      onSave={()=>{}}/>
+};

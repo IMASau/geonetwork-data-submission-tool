de699a2936ecf7af166aa7db9aa6026893a72410
Ben Phelps <biphelps@condense.com.au>
2021-10-07 14:11:24 +1100

    Fix broken story for select field
    Options is a required property

--- frontend/src/js/ui/components/SelectField/SelectValueField.stories.jsx
+++ frontend/src/js/ui/components/SelectField/SelectValueField.stories.jsx
@@ -34,8 +34,8 @@ SimpleField.args = {
     getLabel: (option => option.label)
 };
 
-export const FieldDiabledState = FieldTemplate.bind({});
-FieldDiabledState.args = {
+export const FieldDisabledState = FieldTemplate.bind({});
+FieldDisabledState.args = {
     value: options[0].value,
     options: options,
     disabled: true,
@@ -65,6 +65,7 @@ FieldWithInvalidValue.args = {
 export const EmptyFieldWithPlaceholder = FieldTemplate.bind({});
 EmptyFieldWithPlaceholder.args = {
     placeholder: "This is the placeholder",
+    options: options,
     getValue: (option => option.value),
     getLabel: (option => option.label)
 };

c34a5fb1c83d97e032a391804866abb6823521b5
Oliver George <oliver@condense.com.au>
2021-10-13 10:26:14 +1100

    Update get-block-props-sub
    Now merges config into returned data
    
    Helps to simplify ceremony in views

--- frontend/src/cljs/metcalf4/subs.cljs
+++ frontend/src/cljs/metcalf4/subs.cljs
@@ -22,12 +22,13 @@
 
 ; FIXME: leaking empty strings for date values from payload.forms.data
 (defn get-block-props-sub
-  [state [_ {:keys [data-path]}]]
+  "take config and merge with block props"
+  [state [_ {:keys [data-path] :as config}]]
   (s/assert (s/nilable ::utils4/data-path) data-path)
   (when (vector? data-path)
     (s/assert some? state)
     (let [path (blocks/block-path data-path)]
-      (get-in state (conj path :props)))))
+      (merge config (get-in state (conj path :props))))))
 
 (defn get-block-data-sub
   [state [_ {:keys [data-path]}]]

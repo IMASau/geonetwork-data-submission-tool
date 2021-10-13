02c6cfae95633b4842b8f60118f6c8ee623a5a23
Oliver George <oliver@condense.com.au>
2021-10-13 11:30:25 +1100

    split out settings data

--- frontend/src/cljs/metcalf4/components.cljs
+++ frontend/src/cljs/metcalf4/components.cljs
@@ -860,9 +860,13 @@
       :loadOptions   #(utils4/fetch-post {:uri uri :body {:query %}})
       :onChange      #(rf/dispatch [::list-option-picker-change config %])}]))
 
+(def async-table-list-option-picker-settings
+  {:req-ks [:uri :valueKey :labelKey :columns]
+   :opt-ks [:placeholder]})
+
 (defn async-table-list-option-picker
   [config]
-  (let [config (massage-config {:req-ks [:uri :valueKey :labelKey :columns] :opt-ks [:placeholder]} config)
+  (let [config (massage-config async-table-list-option-picker-settings config)
         props @(rf/subscribe [::get-block-props config])
         {:keys [placeholder uri disabled errors show-errors valueKey labelKey columns]} props
         hasError (when (and show-errors (seq errors)) true)]

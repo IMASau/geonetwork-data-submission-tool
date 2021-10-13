1761d804332a7f8246e70ef10d9bf04895c826cb
Oliver George <oliver@condense.com.au>
2021-10-13 15:51:51 +1100

    Update low-code/render-template
    warn if symbols not resolved

--- frontend/src/cljs/metcalf4/low_code.cljs
+++ frontend/src/cljs/metcalf4/low_code.cljs
@@ -5,8 +5,16 @@
 (def ^:dynamic template-registry {})
 (def ^:dynamic not-found-hiccup '[:div "not found"])
 
+(defn reporter
+  [x]
+  (when (symbol? x)
+    (js/console.warn (str "Symbol not resolved: " (pr-str x))))
+  x)
+
 (defn render-template
   [{:keys [template-id variables]}]
   (let [form (get template-registry template-id not-found-hiccup)
         smap (merge component-registry variables)]
-    (walk/postwalk-replace smap form)))
+    (walk/postwalk
+      (fn [x] (reporter (if (contains? smap x) (smap x) x)))
+      form)))

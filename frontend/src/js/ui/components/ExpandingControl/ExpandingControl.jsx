077439b0e3ac167a6cc6a4ad921098ebdf001b2b
Oliver George <oliver@condense.com.au>
2021-10-15 13:56:53 +1100

    Fix ExpandingControl.propTypes

--- frontend/src/js/ui/components/ExpandingControl/ExpandingControl.jsx
+++ frontend/src/js/ui/components/ExpandingControl/ExpandingControl.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import {requiredLabelInfo} from "../utils";
 
 
-export function ExpandingControl({label, required, hasError, disabled, children}) {
+export function ExpandingControl({label, required, children}) {
     const [isOpen, setOpen] = React.useState(false);
     return (
         <div className="ExpandingControl">
@@ -24,12 +24,6 @@ export function ExpandingControl({label, required, hasError, disabled, children}
 
 ExpandingControl.propTypes = {
     label: PropTypes.string.isRequired,
-    value: PropTypes.bool,
-    disabled: PropTypes.bool,
     required: PropTypes.bool,
-    hasError: PropTypes.bool,
-    children: PropTypes.oneOfType([
-        PropTypes.arrayOf(PropTypes.node),
-        PropTypes.node
-    ]).isRequired
+    children: PropTypes.node.isRequired
 }

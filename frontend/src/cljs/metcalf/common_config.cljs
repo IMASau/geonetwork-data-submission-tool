0c73176e16bd537e32002db51fc6d7378420158f
Oliver George <oliver@condense.com.au>
2021-10-17 09:53:17 +1100

    unused event handler

--- frontend/src/cljs/metcalf/common_config.cljs
+++ frontend/src/cljs/metcalf/common_config.cljs
@@ -35,7 +35,6 @@
 (rf/reg-event-fx ::handlers3/-load-api-options handlers3/-load-api-options)
 (rf/reg-event-fx :handlers/load-error-page handlers3/load-error-page)
 (rf/reg-event-fx :handlers/load-errors handlers3/load-errors)
-(rf/reg-event-fx :handlers/load-es-options handlers3/load-es-options)
 (rf/reg-event-fx ::handlers3/-load-es-options handlers3/-load-es-options)
 (rf/reg-event-fx :metcalf4.components/lodge-button-click handlers3/lodge-click)
 (rf/reg-event-fx ::handlers3/-lodge-save-error handlers3/lodge-error)

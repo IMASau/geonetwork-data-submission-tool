38591f6d6955451a6877befcc82d6397d5c526b1
olivergeorge <oliver@condense.com.au>
2021-10-16 22:05:30 +1100

    unused vars

--- frontend/src/cljs/interop/cuerdas.cljs
+++ frontend/src/cljs/interop/cuerdas.cljs
@@ -1,7 +1,4 @@
 (ns interop.cuerdas
   (:require [cuerdas.core :as cuerdas]))
 
-(def starts-with? cuerdas/starts-with?)
-(def includes? cuerdas/includes?)
-(def lower cuerdas/lower)
 (def human cuerdas/human)

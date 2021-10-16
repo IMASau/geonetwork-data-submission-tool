0899d9bc4f16a3522dbd023b1c09afa8673915c7
olivergeorge <oliver@condense.com.au>
2021-10-16 22:05:09 +1100

    unused vars

--- frontend/src/cljs/interop/react_select.cljs
+++ frontend/src/cljs/interop/react_select.cljs
@@ -5,16 +5,6 @@
             ["react-select/async-creatable" :default AsyncCreatable]
             ["react-select/creatable" :default Creatable]))
 
-(assert React)
-(assert ReactSelect)
-(assert Select)
-(assert AsyncSelect)
-(assert AsyncCreatable)
-(assert Creatable)
-
 (def ReactSelect* (React/createFactory Select))
-(def SelectComponentsOption* (React/createFactory ReactSelect/components.Option))
-(def SelectComponentsValueContainer* (React/createFactory ReactSelect/components.ValueContainer))
 (def ReactSelectAsync* (React/createFactory AsyncSelect))
-(def ReactSelectCreatable* (React/createFactory Creatable))
 (def ReactSelectAsyncCreatable* (React/createFactory AsyncCreatable))

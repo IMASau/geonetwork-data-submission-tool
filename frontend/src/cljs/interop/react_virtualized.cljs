9a47ee1f347c055c4cb491f319494cd2dc15386d
olivergeorge <oliver@condense.com.au>
2021-10-16 22:04:52 +1100

    unused vars

--- frontend/src/cljs/interop/react_virtualized.cljs
+++ frontend/src/cljs/interop/react_virtualized.cljs
@@ -1,12 +1,1 @@
-(ns interop.react-virtualized
-  (:require ["react" :as React]
-            ["react-virtualized" :as ReactVirtualized]
-            ["react-window" :as ReactWindow]))
-
-(assert React)
-(assert ReactVirtualized)
-(assert ReactWindow)
-
-(def ReactWindow* (React/createFactory ReactWindow/FixedSizeList))
-(def AutoSizer* (React/createFactory ReactVirtualized/AutoSizer))
-(def VirtualScroll* (React/createFactory ReactVirtualized/VirtualScroll))
+(ns interop.react-virtualized)

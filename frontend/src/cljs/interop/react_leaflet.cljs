04065cbc22c865bb4045b125cf01688a695a89d8
olivergeorge <oliver@condense.com.au>
2021-10-16 22:09:21 +1100

    unused ns

--- frontend/src/cljs/interop/react_leaflet.cljs
+++
@@ -1,7 +0,0 @@
-(ns interop.react-leaflet
-  (:refer-clojure :exclude [map])
-  (:require ["leaflet"]
-            ["leaflet-draw"]
-            ["react-leaflet" :as ReactLeaflet]
-            ["react-leaflet-draw" :refer (EditControl)]
-            [reagent.core :as r]))

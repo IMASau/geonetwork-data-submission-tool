6905ceab89b5f30243c26c75ae735071deb97438
Oliver George <oliver@condense.com.au>
2021-10-17 09:26:36 +1100

    unused event

--- frontend/src/cljs/metcalf/common_config.cljs
+++ frontend/src/cljs/metcalf/common_config.cljs
@@ -64,7 +64,6 @@
 (rf/reg-event-fx :handlers/update-boxes handlers3/update-boxes)
 (rf/reg-event-fx :handlers/update-dp-term handlers3/update-dp-term)
 (rf/reg-event-fx :handlers/update-method-name handlers3/update-method-name)
-(rf/reg-event-fx :handlers/update-method-term handlers3/update-method-term)
 (rf/reg-event-fx ::views3/nasa-list-select-field-change handlers3/update-nasa-list-value)
 (rf/reg-event-fx ::views3/person-input-field-picker-change handlers3/update-person)
 (rf/reg-event-fx ::views3/person-list-field-change handlers3/update-person)

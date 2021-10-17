d9ead4a157fdf814ab9c2fddc66ba168e7747b63
Oliver George <oliver@condense.com.au>
2021-10-17 16:10:10 +1100

    Tidy up metcalf3.subs
    * remove unused subs/helpers
    * consistent ns aliases

--- frontend/src/cljs/metcalf3/subs.cljs
+++ frontend/src/cljs/metcalf3/subs.cljs
@@ -1,45 +1,15 @@
 (ns metcalf3.subs
-  (:require [cljs.spec.alpha :as s]
-            [clojure.set :as set]
+  (:require [clojure.set :as set]
             [clojure.string :as string]
-            [interop.moment :as moment]
-            [metcalf3.logic :as logic]))
+            [metcalf3.logic :as logic3]))
 
 (defn get-derived-state
   [db _]
-  (logic/derived-state db))
-
-(defn form-dirty?
-  [db _]
-  (get-in db [:form :dirty]))
-
-(defn is-page-name-nil?
-  [db _]
-  (-> db :page :name nil?))
+  (logic3/derived-state db))
 
 (defn get-derived-path
   [db [_ path]]
   (get-in db path))
-
-(defn get-input-field-with-label-props
-  [db [_ path]]
-  (let [{:keys [value disabled errors show-errors]} (get-in db path)
-        error-help (when (and show-errors (seq errors))
-                     (string/join ". " errors))]
-    {:value     value
-     :disabled  disabled
-     :hasError  (boolean error-help)
-     :errorText error-help}))
-
-(defn get-textarea-field-with-label-props
-  [db [_ path]]
-  (let [{:keys [value disabled errors show-errors]} (get-in db path)
-        error-help (when (and show-errors (seq errors))
-                     (string/join ". " errors))]
-    {:value     value
-     :disabled  disabled
-     :hasError  (boolean error-help)
-     :errorText error-help}))
 
 (defn get-page-props
   [db _]
@@ -60,7 +30,7 @@
 (defn get-dashboard-props
   [db _]
   (let [{:keys [status-filter]
-         :or   {status-filter logic/active-status-filter}
+         :or   {status-filter logic3/active-status-filter}
          :as   page} (get-in db [:page])
         {:keys [documents status urls user]} (get-in db [:context])
         status-freq (frequencies (map :status documents))
@@ -82,30 +52,6 @@
      :relevant-status-filter relevant-status-filter
      :urls                   urls}))
 
-(def edit-tabs
-  [{:id :data-identification :text "Data identification"}
-   {:id :what :text "What"}
-   {:id :when :text "When"}
-   {:id :where :text "Where"}
-   {:id :how :text "How"}
-   {:id :who :text "Who"}
-   {:id :about :text "About"}
-   {:id :upload :text "Data sources"}
-   {:id :lodge :text "Lodge"}])
-
-(defn get-edit-tab-props
-  [[page derived-db]]
-  (let [selected-tab (get page :tab :data-identification)]
-    {:selected-tab selected-tab
-     :tab-props    (mapv
-                     (fn [{:keys [id text]}]
-                       (let [progress (get-in derived-db [:progress])
-                             error-count (get-in progress [:page-errors id])]
-                         {:id          id
-                          :title       text
-                          :has-errors? (and error-count (> error-count 0))}))
-                     edit-tabs)}))
-
 (defn get-progress-props
   [derived-db _]
   (let [{:keys [fields empty errors]} (:progress derived-db)
@@ -113,48 +59,6 @@
     (when (pos-int? fields)
       {:can-submit? can-submit?
        :value       (/ (- fields empty) fields)})))
-
-(defn get-date-field-props
-  [derived-db [_ path]]
-  (let [{:keys [label help required disabled value show-errors errors minDate maxDate]} (get-in derived-db path)
-        value (if (= value "") nil value)
-        error-help (when (and show-errors (seq errors))
-                     (string/join ". " errors))]
-    {:label      label
-     :labelInfo  (when required "*")
-     :helperText (or error-help help)
-     :value      (when value (moment/to-date (moment/moment value "YYYY-MM-DD")))
-     :disabled   disabled
-     :change-v   [:date-field/value-change path]
-     :minDate    (s/assert (s/nilable inst?) minDate)
-     :maxDate    (s/assert (s/nilable inst?) maxDate)
-     :intent     (when error-help "danger")}))
-
-(defn get-date-field-with-label-props
-  [derived-db [_ path]]
-  (let [{:keys [required disabled value show-errors errors minDate maxDate]} (get-in derived-db path)
-        value (if (= value "") nil value)
-        error-help (when (and show-errors (seq errors))
-                     (string/join ". " errors))]
-    {:value     (when value (moment/to-date (moment/moment value "YYYY-MM-DD")))
-     :disabled  disabled
-     :hasError  (boolean error-help)
-     :errorText error-help}))
-
-(defn get-textarea-field-props
-  [derived-db [_ path]]
-  (let [{:keys [label help required disabled value show-errors errors placeholder maxlength]} (get-in derived-db path)
-        error-help (when (and show-errors (seq errors))
-                     (string/join ". " errors))]
-    {:label       label
-     :labelInfo   (when required "*")
-     :helperText  (or error-help help)
-     :value       (or value "")
-     :disabled    disabled
-     :maxlength   maxlength
-     :placeholder placeholder
-     :change-v    [:textarea-field/value-change path]
-     :intent      (when error-help "danger")}))
 
 (defn get-textarea-field-many-props
   [derived-db [_ path field]]
@@ -175,14 +79,6 @@
 (defn get-form-tick
   [db _]
   (get db :form/tick 0))
-
-(defn get-menuitems
-  [db _]
-  (let [{:keys [guide_pdf roadmap_pdf releasenotes_url]} (get-in db [:context :site])]
-    (seq (cond-> []
-           guide_pdf (conj ["Guide" [:help-menu/open guide_pdf]])
-           roadmap_pdf (conj ["Roadmap" [:help-menu/open roadmap_pdf]])
-           releasenotes_url (conj ["Release Notes" [:help-menu/open releasenotes_url]])))))
 
 (defn platform-selected?
   [db [_ form-position]]

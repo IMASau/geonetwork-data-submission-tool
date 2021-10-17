ad0cf8deda83023ce5d005d841bb1427d8ab0d40
Oliver George <oliver@condense.com.au>
2021-10-18 08:32:02 +1100

    Update handlers4/list-add-with-defaults-click-handler2 to accept item-defaults

--- frontend/src/cljs/metcalf4/handlers.cljs
+++ frontend/src/cljs/metcalf4/handlers.cljs
@@ -55,13 +55,13 @@
 
 (defn list-add-with-defaults-click-handler2
   [{:keys [db]} [_ config]]
-  (let [{:keys [form-id data-path value-path added-path]} config
-        defaults (-> {}
-                     (assoc-in value-path (str (random-uuid)))
-                     (assoc-in added-path true))]
+  (let [{:keys [form-id data-path value-path added-path item-defaults]} config
+        item-data (-> item-defaults
+                      (assoc-in value-path (str (random-uuid)))
+                      (assoc-in added-path true))]
     (-> {:db db}
         (actions/save-snapshot-action form-id)
-        (actions/add-item-action form-id data-path defaults)
+        (actions/add-item-action form-id data-path item-data)
         (actions/select-last-item-action form-id data-path))))
 
 (defn list-add-with-defaults-click-handler

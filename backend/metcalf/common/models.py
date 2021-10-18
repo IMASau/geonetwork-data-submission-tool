4919b213aea925734668ecddcd9ed74589287e3e
Oliver George <oliver@condense.com.au>
2021-10-18 12:11:32 +1100

    give document model a nice related_name for contributors

--- backend/metcalf/common/models.py
+++ backend/metcalf/common/models.py
@@ -44,7 +44,7 @@ class AbstractDataFeed(models.Model):
 
 class AbstractContributor(models.Model):
     id = models.AutoField(primary_key=True)
-    document = models.ForeignKey("Document", on_delete=models.CASCADE)
+    document = models.ForeignKey("Document", on_delete=models.CASCADE, related_name="contributors")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:

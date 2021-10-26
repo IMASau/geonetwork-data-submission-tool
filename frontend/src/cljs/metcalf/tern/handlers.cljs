ba79c269a93bd2b08168573ac9b352644c2f448f
Oliver George <oliver@condense.com.au>
2021-10-26 20:29:20 +1100

    check data against schema in init-db

--- frontend/src/cljs/metcalf/tern/handlers.cljs
+++ frontend/src/cljs/metcalf/tern/handlers.cljs
@@ -1,13 +1,16 @@
 (ns metcalf.tern.handlers
   (:require [metcalf3.logic :as logic3]
             [metcalf4.actions :as actions]
-            [clojure.edn :as edn]))
+            [clojure.edn :as edn]
+            [metcalf4.schema :as schema]))
 
 (defn init-db
   [_ [_ payload]]
   (let [ui-data (some-> payload :ui_payload edn/read-string)
-        editor-tabs (:low-code/edit-tabs ui-data)]
-    (-> {:db (-> (logic3/initial-state payload)
-                 (cond-> editor-tabs (assoc :low-code/edit-tabs editor-tabs)))
+        editor-tabs (:low-code/edit-tabs ui-data)
+        db (-> (logic3/initial-state payload)
+               (cond-> editor-tabs (assoc :low-code/edit-tabs editor-tabs)))]
+    (schema/assert-schema-data (:form db))
+    (-> {:db db
          :fx [[:ui/setup-blueprint]
               [:metcalf4.low-code/init! ui-data]]})))

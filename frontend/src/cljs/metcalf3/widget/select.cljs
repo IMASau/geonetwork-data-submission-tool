fe4a741588b4e9de331a7b23480ecc09b059b5d3
Oliver George <oliver@condense.com.au>
2021-10-29 16:40:19 +1100

    Unused ns

--- frontend/src/cljs/metcalf3/widget/select.cljs
+++
@@ -1,6 +0,0 @@
-(ns metcalf3.widget.select
-  (:require [interop.react-select :refer [ReactSelect* ReactSelectAsync*]]
-            [metcalf3.utils :as utils]))
-
-(defn normalize-props [props]
-  (utils/clj->js* (update props :options utils/clj->js* 1) 1))

ab617318f5eca2e44e919859c96f7980d5c8b53d
olivergeorge <oliver@condense.com.au>
2021-11-04 21:02:25 +1100

    remove metcalf3.widget.tree utils

--- frontend/src/cljs/metcalf3/widget/tree.cljs
+++ frontend/src/cljs/metcalf3/widget/tree.cljs
@@ -1,8 +1,2 @@
 (ns metcalf3.widget.tree
   (:require [reagent.core :as r]))
-
-(defn node-parent?
-  ([{:keys [lft rgt tree_id]} parent]
-   (and (= (:tree_id parent) tree_id)
-        (< (:lft parent) lft)
-        (> (:rgt parent) rgt))))

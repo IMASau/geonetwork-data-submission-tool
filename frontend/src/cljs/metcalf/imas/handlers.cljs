58bff945f6eec8bc202300d57351600e18175956
olivergeorge <oliver@condense.com.au>
2021-11-04 21:47:00 +1100

    hook up create-form

--- frontend/src/cljs/metcalf/imas/handlers.cljs
+++ frontend/src/cljs/metcalf/imas/handlers.cljs
@@ -1,6 +1,7 @@
 (ns metcalf.imas.handlers
   (:require [metcalf3.logic :as logic3]
-            [metcalf4.schema :as schema]))
+            [metcalf4.schema :as schema]
+            [metcalf4.actions :as actions]))
 
 (defn init-db
   [_ [_ payload]]
@@ -8,8 +9,9 @@
         db (update db' :api #(dissoc % :api/ternparameters :api/qudtunits :api/terninstruments
                                      :api/horizontalresolution :api/ternplatforms :api/topiccategory))]
     (schema/assert-schema-data (:form db))
-    {:db         db
-     :fx         [[:ui/setup-blueprint]]
-     ; TODO: use action
-     :dispatch-n (for [api-key (keys (get db :api))]
-                   [::-init-db-load-api-options [:api api-key]])}))
+    (-> {:db         db
+         :fx         [[:ui/setup-blueprint]]
+         ; TODO: use action
+         :dispatch-n (for [api-key (keys (get db :api))]
+                       [::-init-db-load-api-options [:api api-key]])}
+        (actions/init-create-form-action))))

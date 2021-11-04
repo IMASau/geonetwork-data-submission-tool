850075ec5be2838ccfd68dd9d823f14f9dc5b9e4
olivergeorge <oliver@condense.com.au>
2021-11-04 21:02:06 +1100

    remove metcalf3.widget.tree utils

--- frontend/src/cljs/metcalf3/widget/tree.cljs
+++ frontend/src/cljs/metcalf3/widget/tree.cljs
@@ -6,15 +6,3 @@
    (and (= (:tree_id parent) tree_id)
         (< (:lft parent) lft)
         (> (:rgt parent) rgt))))
-
-(defn node-parents
-  [nodes node]
-  (->> nodes
-       (filter (partial node-parent? node))
-       (sort-by :lft)))
-
-(defn is-selected?
-  "This helper allows for value to appear in tree multiple times"
-  [value-key value option]
-  (= (get value value-key)
-     (get option value-key)))

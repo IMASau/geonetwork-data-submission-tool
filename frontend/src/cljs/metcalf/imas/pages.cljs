644286dd48d9ada3bb4c5a3f33e4bd5765897011
olivergeorge <oliver@condense.com.au>
2021-11-07 15:43:48 +1100

    use views4/dashboard

--- frontend/src/cljs/metcalf/imas/pages.cljs
+++ frontend/src/cljs/metcalf/imas/pages.cljs
@@ -37,8 +37,16 @@
            :on-save    #(rf/dispatch [:app/modal-dialog-confirm-save])}]
          nil))
      (case page-name
-       "404" [views3/PageView404 nil]
-       "Error" [views3/PageViewError nil]
-       "Edit" [views3/PageViewEdit nil]
-       "Dashboard" [views3/dashboard nil]
+       "404"
+       [views3/PageView404 nil]
+       "Error"
+       [views3/PageViewError nil]
+       "Edit"
+       [views3/PageViewEdit nil]
+       "Dashboard"
+       [views4/dashboard
+        {:dashboard-props                @(rf/subscribe [::get-dashboard-props])
+         :dashboard-create-click         #(rf/dispatch [::dashboard-create-click])
+         :dashboard-show-all-click       #(rf/dispatch [::dashboard-show-all-click])
+         :dashboard-toggle-status-filter #(rf/dispatch [::dashboard-toggle-status-filter %])}]
        nil)]))

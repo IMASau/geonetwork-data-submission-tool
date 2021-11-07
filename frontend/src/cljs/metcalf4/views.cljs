c71e871c79e748f396bdcfbcc10a879cb1bd1307
olivergeorge <oliver@condense.com.au>
2021-11-07 15:43:41 +1100

    add views4/dashboard

--- frontend/src/cljs/metcalf4/views.cljs
+++ frontend/src/cljs/metcalf4/views.cljs
@@ -3,7 +3,8 @@
             [goog.object :as gobj]
             [interop.react-imask :as react-imask]
             [metcalf3.utils :as utils3]
-            [metcalf3.widget.modal :as modal]))
+            [metcalf3.widget.modal :as modal]
+            [metcalf3.views :as views3]))
 
 ; For pure views only, no re-frame subs/handlers
 
@@ -54,3 +55,50 @@
     :modal-body   message
     :on-dismiss   on-dismiss
     :on-save      on-save}])
+
+(defn dashboard
+  [{:keys [dashboard-props
+           dashboard-create-click
+           dashboard-show-all-click
+           dashboard-toggle-status-filter]}]
+  (let [{:keys [filtered-docs status-filter has-documents? status-freq status relevant-status-filter]} dashboard-props]
+    [:div
+     [views3/navbar]
+     [:div.container
+      [:span.pull-right {:style {:margin-top 18}} [views3/NewDocumentButton]]
+      [:h1 "My Records"]
+      [:div.row
+       [:div.col-sm-9
+        (-> [:div.list-group]
+            (into (for [filtered-doc filtered-docs]
+                    ^{:key (:url filtered-doc)} [views3/DocumentTeaser filtered-doc]))
+            (conj (if has-documents?
+                    [:a.list-group-item {:on-click dashboard-create-click}
+                     [:span.glyphicon.glyphicon-star.pull-right]
+                     [:p.lead.list-group-item-heading " My first record "]
+                     [:p.list-group-item-text "Welcome! Since you’re new here, "
+                      [:span {:style {:text-decoration "underline"}} "Click here"] " to get started."]]
+                    (when (empty? filtered-docs)
+                      (if (= status-filter #{"Draft" "Submitted"})
+                        [:div
+                         [:p "You don't have any active records: "
+                          [:a {:on-click dashboard-show-all-click}
+                           "show all documents"] "."]
+                         [views3/NewDocumentButton]]
+                        [:div
+                         [:p "No documents match your filter: "
+                          [:a {:on-click dashboard-show-all-click}
+                           "show all documents"] "."]
+                         [views3/NewDocumentButton]])))))]
+       [:div.col-sm-3
+        (when-not (empty? status-freq)
+          (into [:div]
+                (for [[status-id sname] status]
+                  (let [freq (get status-freq status-id)]
+                    [:div [:label
+                           [:input {:type      "checkbox"
+                                    :disabled  (not freq)
+                                    :checked   (contains? relevant-status-filter status-id)
+                                    :on-change #(dashboard-toggle-status-filter {:status-id status-id :status-filter status-filter})}]
+                           " " sname
+                           (when freq [:span.freq " (" freq ")"])]]))))]]]]))

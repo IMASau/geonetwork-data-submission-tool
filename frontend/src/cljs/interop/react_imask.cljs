67231902788affb03cc06d0101e4599528f13cf5
olivergeorge <oliver@condense.com.au>
2021-11-08 22:20:54 +1100

    unused ns

--- frontend/src/cljs/interop/react_imask.cljs
+++
@@ -1,7 +0,0 @@
-(ns interop.react-imask
-  (:require ["react-imask" :refer (IMaskInput)]
-            [reagent.core :as r]))
-
-(assert IMaskInput)
-
-(def masked-input (r/adapt-react-class IMaskInput))

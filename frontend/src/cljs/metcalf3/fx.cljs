87a2415f0a302610ee91555bd9a327776c7ddb93
olivergeorge <oliver@condense.com.au>
2021-11-09 09:42:07 +1100

    normalise

--- frontend/src/cljs/metcalf3/fx.cljs
+++ frontend/src/cljs/metcalf3/fx.cljs
@@ -88,18 +88,7 @@
          :params          #js {:transition "submit"}
          :response-format :json}))
 
-(defn save-current-document
-  [{:keys [url data success-v error-v]}]
-  (POST url
-        {:error-handler   #(rf/dispatch (conj error-v %))
-         :format          :json
-         :handler         #(rf/dispatch (conj success-v %))
-         :headers         {"X-CSRFToken" (get-csrf)}
-         :keywords?       true
-         :params          (clj->js data)
-         :response-format :json}))
-
-(defn archive-current-document
+(defn post-json-data
   [{:keys [url data success-v error-v]}]
   (POST url
         {:error-handler   #(rf/dispatch (conj error-v %))

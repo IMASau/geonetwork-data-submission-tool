c11b572e5a9a55a1ae7c0c2b185690bbe6707823
olivergeorge <oliver@condense.com.au>
2021-11-08 21:56:11 +1100

    unused sub

--- frontend/src/cljs/metcalf/tern/config.cljs
+++ frontend/src/cljs/metcalf/tern/config.cljs
@@ -54,7 +54,6 @@
 (rf/reg-sub ::components4/create-document-modal-can-save? subs4/create-document-modal-can-save?)
 (rf/reg-sub ::components4/get-block-data subs4/form-state-signal subs4/get-block-data-sub)
 (rf/reg-sub ::components4/get-block-props subs4/form-state-signal subs4/get-block-props-sub)
-(rf/reg-sub ::components4/get-data-schema subs4/get-data-schema-sub)
 (rf/reg-sub ::components4/get-yes-no-field-props subs4/form-state-signal subs4/get-block-props-sub)
 (rf/reg-sub ::low-code/get-data-schema subs4/get-data-schema-sub)
 (rf/reg-sub ::subs4/get-form-state subs4/get-form-state)

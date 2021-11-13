2d6639894c021624bba5efbba2f53ad7829cb549
olivergeorge <oliver@condense.com.au>
2021-11-13 13:51:35 +1100

    tidy

--- frontend/src/cljs/metcalf/imas/handlers.cljs
+++ frontend/src/cljs/metcalf/imas/handlers.cljs
@@ -7,14 +7,8 @@
   [_ [_ payload]]
   (let [db (logic3/initial-state payload)]
     (schema4/assert-schema-data (:form db))
-
     (-> {:db         db
-         :fx         [[:ui/setup-blueprint]]
-         ; TODO: use action
-         :dispatch-n (for [api-key (keys (get db :api))]
-                       [::-init-db-load-api-options [:api api-key]])}
-
+         :fx         [[:ui/setup-blueprint]]}
         ;(actions4/load-page-action payload)
         ;(actions4/load-form-action payload)
-
         (actions4/init-create-form-action payload))))

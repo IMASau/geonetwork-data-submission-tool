1d4325911ff3bf7a4b95f8a80aabae0ff93f11ea
Oliver George <oliver@condense.com.au>
2021-11-15 16:25:02 +1100

    move dynaconf into new settings file

---
+++ backend/metcalf/imas/webapp/dynaconf_settings.py
@@ -0,0 +1,14 @@
+from .settings import *
+
+# HERE STARTS DYNACONF EXTENSION LOAD (Keep at the very bottom of settings.py)
+# Read more at https://dynaconf.readthedocs.io/en/latest/guides/django.html
+import dynaconf  # noqa
+
+settings = dynaconf.DjangoDynaconf(
+    __name__,
+    # ENVVAR_PREFIX_FOR_DYNACONF='DST',
+    # ENVVAR_FOR_DYNACONF="DST_SETTINGS",
+    validators=[dynaconf.Validator("ELASTICSEARCH_VERIFY_SSL", is_type_of=bool)],
+)  # noqa
+# HERE ENDS DYNACONF EXTENSION LOAD (No more code below this line)
+

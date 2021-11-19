ba32941609dad4bdaed210b13c179472d9303704
olivergeorge <oliver@condense.com.au>
2021-11-19 11:38:09 +1100

    Remove contributor access out of views

--- backend/metcalf/imas/frontend/views.py
+++ backend/metcalf/imas/frontend/views.py
@@ -86,7 +86,7 @@ def user_status_list():
 @login_required
 def dashboard(request):
     docs = (Document.objects
-            .filter(Q(owner=request.user) | Q(contributors__user=request.user))
+            .filter(Q(owner=request.user))
             .exclude(status=Document.DISCARDED))
     payload = JSONRenderer().render({
         "context": {

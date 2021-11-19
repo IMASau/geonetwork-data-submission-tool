65a86994297555cfc57ab9056224e7c6395075be
olivergeorge <oliver@condense.com.au>
2021-11-19 11:35:36 +1100

    Remove Contributor model from admin

--- backend/metcalf/imas/backend/admin.py
+++ backend/metcalf/imas/backend/admin.py
@@ -89,10 +89,6 @@ class DocumentAttachmentInline(admin.TabularInline):
     model = models.DocumentAttachment
 
 
-class ContributorInline(admin.TabularInline):
-    model = models.Contributor
-
-
 def bulk_scheduled(modeladmin, request, queryset):
     for datafeed in queryset.filter(state=models.DataFeed.IDLE):
         datafeed.schedule()
@@ -149,7 +145,7 @@ class DocumentAdmin(FSMTransitionMixin, admin.ModelAdmin):
     search_fields = ['title', 'owner__username', 'owner__email', 'uuid']
     fsm_field = ['status', ]
     readonly_fields = ['status', 'action_links', 'submission_note']
-    inlines = [DocumentAttachmentInline, ContributorInline]
+    inlines = [DocumentAttachmentInline]
     fieldsets = [
         (None, {'fields': ('title', 'template', 'owner', 'status', 'submission_note')}),
         ('Export', {'fields': ('action_links',)}),

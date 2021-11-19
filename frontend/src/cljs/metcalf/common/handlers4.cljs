329763c426c32c9342b5ab71689d0113c2b01d7a
olivergeorge <oliver@condense.com.au>
2021-11-19 12:03:54 +1100

    whitespace

--- frontend/src/cljs/metcalf/common/handlers4.cljs
+++ frontend/src/cljs/metcalf/common/handlers4.cljs
@@ -123,32 +123,32 @@
         new-field-path (conj data-path idx)]
     (-> {:db db}
         (actions4/add-value-action form-id data-path initial-data)
-        (actions4/open-modal-action {:type    :m4/table-modal-add-form
-                              :form           coord-field
-                              :path           new-field-path
-                              :title          "Geographic Coordinates"
-                              :on-close-click #(on-close idx)
-                              :on-save-click  on-save}))))
+        (actions4/open-modal-action {:type           :m4/table-modal-add-form
+                                     :form           coord-field
+                                     :path           new-field-path
+                                     :title          "Geographic Coordinates"
+                                     :on-close-click #(on-close idx)
+                                     :on-save-click  on-save}))))
 
 (defn boxmap-coordinates-open-edit-modal
   [{:keys [db]} [_ {:keys [ctx coord-field on-delete on-cancel on-save]}]]
   (let [{:keys [data-path]} ctx]
     (-> {:db db}
-        (actions4/open-modal-action {:type     :m4/table-modal-edit-form
-                              :form            coord-field
-                              :path            data-path
-                              :title           "Geographic Coordinates"
-                              :on-delete-click on-delete
-                              :on-close-click  on-cancel
-                              :on-save-click   on-save}))))
+        (actions4/open-modal-action {:type            :m4/table-modal-edit-form
+                                     :form            coord-field
+                                     :path            data-path
+                                     :title           "Geographic Coordinates"
+                                     :on-delete-click on-delete
+                                     :on-close-click  on-cancel
+                                     :on-save-click   on-save}))))
 
 (defn boxmap-coordinates-click-confirm-delete
   [{:keys [db]} [_ on-confirm]]
   (-> {:db db}
-      (actions4/open-modal-action {:type :modal.type/confirm
-                            :title       "Delete"
-                            :message     "Are you sure you want to delete?"
-                            :on-confirm  on-confirm})))
+      (actions4/open-modal-action {:type       :modal.type/confirm
+                                   :title      "Delete"
+                                   :message    "Are you sure you want to delete?"
+                                   :on-confirm on-confirm})))
 
 (defn boxmap-coordinates-list-delete
   [{:keys [db]} [_ ctx idx]]

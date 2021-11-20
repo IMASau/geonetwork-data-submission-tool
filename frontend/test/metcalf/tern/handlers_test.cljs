5895c68c473cde274004427b51c1b541940d26fb
olivergeorge <oliver@condense.com.au>
2021-11-20 20:57:52 +1100

    Experimenting with quick-check based testing

--- frontend/test/metcalf/tern/handlers_test.cljs
+++ frontend/test/metcalf/tern/handlers_test.cljs
@@ -16,27 +16,32 @@
 (s/def ::name string?)
 (s/def ::edit-tab (s/keys :req-un [::id ::name]))
 (s/def ::edit-tabs (s/coll-of ::edit-tab))
-(s/def ::init-db--args-map (s/keys :req-un [::page-name]
-                                   :opt-un [::edit-tabs]))
 (s/def ::db map?)
 (s/def ::event-v (s/cat :eid keyword? :args (s/* any?)))
 (s/def ::fx (s/coll-of (s/nilable ::event-v)))
 (s/def ::fx-ret
   (s/and (s/keys :opt-un [::db ::fx])
-         (fn [ret] (empty? (dissoc ret :fx :db)))))
+         (fn only-fx-db [ret] (empty? (dissoc ret :fx :db)))))
 
 (def init-db--fx-ret
-  (prop/for-all [{:keys [page-name edit-tabs]} (s/gen ::init-db--args-map)]
+  (prop/for-all [{:keys [page-name edit-tabs]}
+                 (s/gen (s/keys :req-un [::page-name]
+                                :opt-un [::edit-tabs]))]
     (let [ui-data (when (seq edit-tabs)
                     {:low-code/edit-tabs edit-tabs})
           payload (cond-> {:page {:name page-name}}
                     (seq ui-data) (assoc :ui_payload (pr-str ui-data)))
           ret (tern-handlers/init-db {:db {}} [:_ payload])]
-      (println ret)
       (and (s/valid? map? (:db ret))
            (s/valid? ::fx-ret ret)))))
 
-(deftest init-db--gen
-  (is (= true (:pass? (tc/quick-check 100 init-db--fx-ret)))))
+(defn qc-ret
+  [{:keys [result] :as data}]
+  (if result {:result true} data))
 
-(comment (tc/quick-check 100 init-db--fx-ret))
+(deftest init-db--gen
+  (is (= {:result true} (qc-ret (tc/quick-check 100 init-db--fx-ret)))))
+
+(comment (tc/quick-check 100 init-db--fx-ret)
+         (init-db--gen)
+         (cljs.test/run-tests))

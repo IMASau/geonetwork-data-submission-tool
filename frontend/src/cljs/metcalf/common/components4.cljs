f816266128c0997d1b010fa3c6d5b305145f5d31
olivergeorge <oliver@condense.com.au>
2021-11-26 14:55:16 +1100

    Use :subs/get-context instead of :subs/get-derived-path [:context]

--- frontend/src/cljs/metcalf/common/components4.cljs
+++ frontend/src/cljs/metcalf/common/components4.cljs
@@ -321,7 +321,7 @@
 
 (defn portal-link
   []
-  (let [{:keys [site]} @(rf/subscribe [:subs/get-derived-path [:context]])
+  (let [{:keys [site]} @(rf/subscribe [:subs/get-context])
         {:keys [portal_title portal_url]} site]
     (if portal_url
       [:a {:href portal_url :target "_blank"} [:span.portal-title portal_title]]

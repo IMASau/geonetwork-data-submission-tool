0e6d6f80e614553e758ea2a7ffba9a9c2f7d6897
olivergeorge <oliver@condense.com.au>
2021-11-26 16:46:54 +1100

    unused vars

--- frontend/src/cljs/metcalf/common/logic3.cljs
+++ frontend/src/cljs/metcalf/common/logic3.cljs
@@ -1,36 +1,3 @@
-(ns metcalf.common.logic3
-  (:require [clojure.string :as string]
-            [metcalf.common.blocks4 :as blocks4]
-            [metcalf.common.logic4 :as logic4]
-            [metcalf.common.rules4 :as rules4]
-            [metcalf.common.utils3 :as utils3]))
+(ns metcalf.common.logic3)
 
 (def active-status-filter #{"Draft" "Submitted"})
-
-; TODO: Use field-zipper.
-; TODO: Store non-field errors (:non_field_errors form)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; https://github.com/Roxxi/clojure-common-utils
-
-(declare mask-map)
-
-(defn mask-map-triage-kv [kv a-mask]
-  (let [[k v] [(key kv) (val kv)]]
-    (when-let [mask-v (get a-mask k)]
-      (cond (fn? mask-v) {k (mask-v v)}
-            (and (map? mask-v) (map? v))
-            {k (mask-map v mask-v)}
-            :else {k v}))))
-
-(defn mask-map
-  "Given a mask-map whose structure is some subset of some-map's
-   structure, extract the structure specified. For a path to be extracted
-   the terminal value in the mask-map must be a non-false yielding value.
-
-   If a function is provided as a terminal value in the mask, the function
-   will be applied to the value in the source location, before being
-   carried over to the resulting map.
-
-   If the mask yields no values, nil will be returned."
-  [some-map map-mask]
-  (apply merge (remove nil? (map #(mask-map-triage-kv % map-mask) some-map))))

8ee4a58abd6df1c7340abe7280b3e77f9e1be5d1
olivergeorge <oliver@condense.com.au>
2021-11-27 17:12:02 +1100

    do away with default-db

--- frontend/src/cljs/metcalf/imas/handlers.cljs
+++ frontend/src/cljs/metcalf/imas/handlers.cljs
@@ -9,20 +9,20 @@
 
     "Dashboard"
     (let [{:keys [context create_form page]} payload]
-      (-> {:db db4/default-db
+      (-> {:db {:modal/stack []
+                :context     context
+                :page        page}
            :fx [[:ui/setup-blueprint]]}
-          (assoc-in [:db :context] context)
-          (assoc-in [:db :page] page)
           (actions4/init-create-form-action create_form)
           (actions4/load-dashboard-document-data payload)))
 
     "Edit"
     (let [{:keys [context form upload_form attachments page]} payload]
-      (-> {:db db4/default-db
+      (-> {:db {:modal/stack []
+                :context     context
+                :upload_form upload_form
+                ;:data data
+                :attachments attachments
+                :page        page}
            :fx [[:ui/setup-blueprint]]}
-          (assoc-in [:db :context] context)
-          (assoc-in [:db :upload_form] upload_form)
-          ;(assoc-in [:db :data] data)
-          (assoc-in [:db :attachments] attachments)
-          (assoc-in [:db :page] page)
           (actions4/load-edit-form-action form)))))

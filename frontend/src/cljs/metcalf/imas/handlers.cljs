d948a248ffae6349fa8c2f441c79b4e9cc51c4d1
olivergeorge <oliver@condense.com.au>
2021-11-27 14:35:31 +1100

    make explicit payload->db behaviour

--- frontend/src/cljs/metcalf/imas/handlers.cljs
+++ frontend/src/cljs/metcalf/imas/handlers.cljs
@@ -8,17 +8,28 @@
   (case (get-in payload [:page :name])
 
     "Dashboard"
-    (-> {:db db4/default-db
-         :fx [[:ui/setup-blueprint]]}
-        (update :db merge payload)
-        (actions4/load-page-action payload)
-        (actions4/init-create-form-action payload)
-        (actions4/load-dashboard-document-data payload))
+    (let [{:keys [context create_form messages page]} payload]
+      (-> {:db db4/default-db
+           :fx [[:ui/setup-blueprint]]}
+          (assoc-in [:db :context] context)
+          (assoc-in [:db :create_form] create_form)
+          (assoc-in [:db :messages] messages)
+          (assoc-in [:db :page] page)
+          (actions4/load-page-action payload)
+          (actions4/init-create-form-action payload)
+          (actions4/load-dashboard-document-data payload)))
 
     "Edit"
-    (-> {:db db4/default-db
-         :fx [[:ui/setup-blueprint]]}
-        (update :db merge payload)
-        (actions4/load-page-action payload)
-        (actions4/load-form-action payload))
-    ))
+    (let [{:keys [context form upload_form messages data attachments theme page]} payload]
+      (-> {:db db4/default-db
+           :fx [[:ui/setup-blueprint]]}
+          (assoc-in [:db :context] context)
+          (assoc-in [:db :form] form)
+          (assoc-in [:db :upload_form] upload_form)
+          (assoc-in [:db :messages] messages)
+          (assoc-in [:db :data] data)
+          (assoc-in [:db :attachments] attachments)
+          (assoc-in [:db :theme] theme)
+          (assoc-in [:db :page] page)
+          (actions4/load-page-action payload)
+          (actions4/load-form-action payload)))))

84cb151b7635bc363197516671e74fb039136c63
Mark Hepburn <Mark.Hepburn@gmail.com>
2021-12-06 22:47:17 +1100

    Whitespace

--- backend/metcalf/tern/backend/viewsets.py
+++ backend/metcalf/tern/backend/viewsets.py
@@ -41,7 +41,7 @@ def duma_update(request, *args, **kwargs):
     path = data['duma_path'].lstrip('.').split('.')
 
     document = get_object_or_404(models.Document, uuid=docid)
-    document_draft =document.latest_draft
+    document_draft = document.latest_draft
     document_data = document_draft.data
 
     updated_doc = xmlutils4.update_user_defined(document_data, data, path)

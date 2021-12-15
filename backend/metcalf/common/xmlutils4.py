2cdbb67951c2b2c5624930e6b319e42c24382e52
Mark Hepburn <Mark.Hepburn@gmail.com>
2021-12-15 14:33:41 +1100

    exportTo: pass data in to the accessor function
    The motivation for this is for exportTo to also accept a function, so
    that it can be used to return the same structure but dependent on the
    data.  The motivating example is the spatial attributes, where
    depending on the units (eg metres, degrees,... ) the xml structure is
    actually different.

--- backend/metcalf/common/xmlutils4.py
+++ backend/metcalf/common/xmlutils4.py
@@ -493,7 +493,7 @@ def data_to_xml(data, xml_node, spec, nsmap, doc_uuid, element_index=0, silent=T
     # export can be false with an exportTo function, i.e. don't do the default export, do this instead
     elif not is_export(spec):
         if has_exportTo(spec):
-            data_to_xml(data=data, xml_node=xml_node, spec=get_exportTo(spec), nsmap=nsmap,
+            data_to_xml(data=data, xml_node=xml_node, spec=get_exportTo(spec, data), nsmap=nsmap,
                         element_index=element_index, silent=silent, fieldKey=fieldKey, doc_uuid=doc_uuid)
     elif is_batch(spec):
         spec, data = spec_data_from_batch(get_batch(spec), data)
@@ -601,7 +601,7 @@ def data_to_xml(data, xml_node, spec, nsmap, doc_uuid, element_index=0, silent=T
                 else:
                     element.set(attr, final_value)
             if has_exportTo(spec):
-                data_to_xml(data=data, xml_node=xml_node, spec=get_exportTo(spec), nsmap=nsmap,
+                data_to_xml(data=data, xml_node=xml_node, spec=get_exportTo(spec, data), nsmap=nsmap,
                             element_index=element_index, silent=silent, fieldKey=fieldKey, doc_uuid=doc_uuid)
 
     if is_postprocess(spec):

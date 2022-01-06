b445b24bace685eb68b3cb5727af3603e5f446bd
Oliver George <oliver@condense.com.au>
2022-01-06 15:09:04 +1100

    async-select-option-columns docstring

--- frontend/src/cljs/metcalf/common/components4.cljs
+++ frontend/src/cljs/metcalf/common/components4.cljs
@@ -784,6 +784,29 @@
    ::low-code4/schema {:type "object" :properties {}}})
 
 (defn async-select-option-columns
+  "This component renders a select control linked to a json data source.  Selected option is
+   displayed as a text label.  Options in dropdown are displayed as columns of values.
+   The value is option data.
+
+   Props configure the component
+   * value-path (vector) - path to value in the option data.  Values must be unique.
+   * label-path (vector) - path to label is in the option data.  Used to render options and selected value.
+   * columns (maps) - column metadata used when rendering options
+     * label-path - where the column label is in the option data
+     * flex - how much space this column should use.
+   * added-path (vector) - path to test if option is user defined.  Used to style control.
+   * placeholder (string) - text to displayed when no option is selected.
+
+   Props to configure the data source
+   * uri (string) - the resource that you wish to fetch data from
+   * results-path - where the result list is in the data source json data payload
+   * search-param - the request parameter name used for searching for matching results
+
+   Logic can control aspects of how the component is rendered using form-id and data-path to access block props.
+   * disabled - styles control to indicate it's disabled
+   * show-errors? - styles control to indicate data entry errors
+   * is-hidden - hides component entirely
+   "
   [config]
   (let [props @(rf/subscribe [::get-block-props config])
         value @(rf/subscribe [::get-block-data config])

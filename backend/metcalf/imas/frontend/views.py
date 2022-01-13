a8f1f62868b7d00ea5ffc9e794636351e9a7355e
Oliver George <oliver@condense.com.au>
2022-01-13 12:34:53 +1100

    data not used

--- backend/metcalf/imas/frontend/views.py
+++ backend/metcalf/imas/frontend/views.py
@@ -549,7 +549,7 @@ def edit(request, uuid):
             },
             "data": {},
         },
-        "data": data,
+        # "data": data,
         "attachments": AttachmentSerializer(doc.attachments.all(), many=True).data,
         # "theme": {"keywordsTheme": {"table": theme_keywords()}},
         # "institutions": [inst.to_dict() for inst in Institution.objects.all()],

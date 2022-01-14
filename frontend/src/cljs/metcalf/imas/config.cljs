0c706cf31b27e44a3a8e0c5f7f14a4f25d7cbcb7
Oliver George <oliver@condense.com.au>
2022-01-14 13:20:27 +1100

    hook up identificationInfo.verticalElement.minimumValue

--- frontend/src/cljs/metcalf/imas/config.cljs
+++ frontend/src/cljs/metcalf/imas/config.cljs
@@ -444,14 +444,15 @@
      ;   :placeholder "Please select"
      ;   :options     [{"label" "Depth (distance below mean sea level)" "value" "EPSG::5715"}
      ;                 {"label" "Altitude (height above mean sea level)" "value" "EPSG::5714"}]}]]
-     ; [m4/form-group
-     ;  {:form-id    [:form]
-     ;   :data-path  ["identificationInfo" "verticalElement" "minimumValue"]
-     ;   :helperText "Shallowest depth / lowest altitude"}
-     ;  [m4/numeric-input-field
-     ;   {:form-id   [:form]
-     ;    :data-path ["identificationInfo" "verticalElement" "minimumValue"]
-     ;    :class     "wauto"}]]
+     [m4/form-group
+      {:form-id    [:form]
+       :data-path  ["identificationInfo" "verticalElement" "minimumValue"]
+       :label      "Minimum"
+       :helperText "Shallowest depth / lowest altitude"}
+      [m4/numeric-input-field
+       {:form-id   [:form]
+        :data-path ["identificationInfo" "verticalElement" "minimumValue"]
+        :class     "wauto"}]]
      ; [m4/form-group
      ;  {:form-id    [:form]
      ;   :data-path  ["identificationInfo" "verticalElement" "maximumValue"]

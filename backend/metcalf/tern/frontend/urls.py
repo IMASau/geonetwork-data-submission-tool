34f065a6e9e77a091a9527d6537f3d639c5545e4
Oliver George <oliver@condense.com.au>
2022-01-24 20:03:57 +1100

    tern doesn't use /api/rolecode

--- backend/metcalf/tern/frontend/urls.py
+++ backend/metcalf/tern/frontend/urls.py
@@ -9,7 +9,7 @@ from metcalf.tern.frontend.viewsets import DocumentInfoViewSet
 
 router = routers.DefaultRouter()
 # router.register(r'institution', viewsets.InstitutionViewSet)
-router.register(r'rolecode', viewsets.RoleCodeViewSet)
+# router.register(r'rolecode', viewsets.RoleCodeViewSet)
 # router.register(r'parametername', viewsets.ParameterNameViewSet)
 # router.register(r'parameterunit', viewsets.ParameterUnitViewSet)
 # router.register(r'parameterinstrument', viewsets.ParameterInstrumentViewSet)

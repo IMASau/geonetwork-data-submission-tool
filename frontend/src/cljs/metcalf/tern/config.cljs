5a46395eb17e97e3995603bc71394e8dc1e323ae
Oliver George <oliver@condense.com.au>
2022-02-10 10:01:50 +1100

    Update topic list styling
    Picked items now use colour coding for user selected values
    
    Ref SD-28

--- frontend/src/cljs/metcalf/tern/config.cljs
+++ frontend/src/cljs/metcalf/tern/config.cljs
@@ -387,10 +387,10 @@
         :placeholder "Provide a descriptive title for the data set including the subject of study, the study location and time period. Example: TERN OzFlux Arcturus Emerald Tower Site 2014-ongoing"}]]
 
      [m4/form-group
-      {:form-id  [:form]
+      {:form-id   [:form]
        :data-path ["parentMetadata"]
-       :label    "Parent Metadata"
-       :toolTip  "Select 'Yes' if there is a parent metadata record associated with the dataset."}
+       :label     "Parent Metadata"
+       :toolTip   "Select 'Yes' if there is a parent metadata record associated with the dataset."}
       [m4/yes-no-field
        {:form-id   [:form]
         :data-path ["parentMetadata" "parentMetadataFlag"]
@@ -435,11 +435,12 @@
                       {"value" "utilitiesCommunication" "label" "Utilities Communication"}
                       {"value" "extraTerrestrial" "label" "Extra Terrestrial"}
                       {"value" "disaster" "label" "Disaster"}]}]
-      [m4/selection-list-simple
-       {:form-id     [:form]
-        :data-path   ["identificationInfo" "topicCategories"]
-        :label-path  ["label"]
-        :value-path  ["value"]}]]
+      [:div.SelectionListItemColoured
+       [m4/selection-list-simple
+        {:form-id    [:form]
+         :data-path  ["identificationInfo" "topicCategories"]
+         :label-path ["label"]
+         :value-path ["value"]}]]]
 
      [:div {:style {:display               "grid"
                     :grid-column-gap       "1em"
@@ -1640,8 +1641,8 @@
          :data-path ["resourceLineage" "statement"]}]]
 
       [m4/form-group
-       {:label    "Method documentation"
-        :toolTip  "The method of production of the dataset. Provide the title and URL of the method documentation."}
+       {:label   "Method documentation"
+        :toolTip "The method of production of the dataset. Provide the title and URL of the method documentation."}
        [m4/selection-list-columns
         {:form-id            [:form]
          :data-path          ["resourceLineage" "onlineMethods"]
@@ -1673,25 +1674,25 @@
 
       ;; How6: Name
       #_[m4/form-group
-       {:form-id    [:form]
-        :data-path  ["resourceLineage" "processStatement"]
-        :label      "Name"
-        :helperText "Provide the name of the method or procedure"}
-       [m4/textarea-field
-        {:form-id     [:form]
-         :data-path   ["resourceLineage" "processStatement"]
-         :placeholder "Provide the name of the method or procedure"}]]
+         {:form-id    [:form]
+          :data-path  ["resourceLineage" "processStatement"]
+          :label      "Name"
+          :helperText "Provide the name of the method or procedure"}
+         [m4/textarea-field
+          {:form-id     [:form]
+           :data-path   ["resourceLineage" "processStatement"]
+           :placeholder "Provide the name of the method or procedure"}]]
 
       ;; How7: Description
       #_[m4/form-group
-       {:form-id    [:form]
-        :data-path  ["resourceLineage" "summary"]
-        :label      "Description"
-        :helperText "Provide a brief description of the method"}
-       [m4/textarea-field
-        {:form-id     [:form]
-         :data-path   ["resourceLineage" "summary"]
-         :placeholder "Provide a brief summary of a single method or procedure"}]]
+         {:form-id    [:form]
+          :data-path  ["resourceLineage" "summary"]
+          :label      "Description"
+          :helperText "Provide a brief description of the method"}
+         [m4/textarea-field
+          {:form-id     [:form]
+           :data-path   ["resourceLineage" "summary"]
+           :placeholder "Provide a brief summary of a single method or procedure"}]]
 
       ;; How7b: list-add free-text entries
       [m4/form-group
@@ -1824,10 +1825,10 @@
      [m4/form-group
       {;:form-id   [:form]
        ;:data-path ["identificationInfo"]
-       :label     "Other constraints"}
+       :label "Other constraints"}
       [m4/simple-list
-       {:form-id [:form]
-        :data-path ["identificationInfo" "otherConstraints"]
+       {:form-id     [:form]
+        :data-path   ["identificationInfo" "otherConstraints"]
         :template-id :about/other-constraints-list-entry}]]
      [m4/form-group
       {:label   "Any other constraints"
@@ -1874,8 +1875,8 @@
      [m4/expanding-control {:label "Associated Documentation (Optional)"}
 
       [m4/form-group
-       {:label    "Publication"
-        :toolTip  "Please provide the title and URL of the publications describing the dataset."}
+       {:label   "Publication"
+        :toolTip "Please provide the title and URL of the publications describing the dataset."}
        [m4/selection-list-columns
         {:form-id            [:form]
          :data-path          ["identificationInfo" "additionalPublications"]
@@ -1975,7 +1976,7 @@
     [:p {:key ?data-path}
      [:i
       [m4/get-data
-       {:form-id ?form-id
+       {:form-id   ?form-id
         :data-path ?data-path}]]]
 
     :upload
@@ -2010,8 +2011,8 @@
 
      [:h2 "Data Services"]
      [m4/form-group
-      {:label    "Distributions"
-       :toolTip  "Provide information (name, URL) about the services for downloading the datasets."}
+      {:label   "Distributions"
+       :toolTip "Provide information (name, URL) about the services for downloading the datasets."}
       [m4/selection-list-columns
        {:form-id            [:form]
         :data-path          ["dataSources"]

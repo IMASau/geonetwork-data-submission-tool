e28f997e843e71e7beb0c103f28a2dc9ce7ca34e
Joshua Groombridge <joshua@condense.com.au>
2022-02-16 15:57:50 +1100

    (SD-33) generated citation text area

--- frontend/src/cljs/metcalf/tern/config.cljs
+++ frontend/src/cljs/metcalf/tern/config.cljs
@@ -2129,6 +2129,15 @@
      [:h2 "10: Lodge Metadata Draft"]
      [:p "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]
 
+     ;; TODO: provide a citation data path
+     [m4/form-group
+      {:form-id   [:form]
+       :data-path ["XXX"]
+       :label     "Generated Citation"}
+      [m4/textarea-field
+       {:form-id   [:form]
+        :data-path ["XXX"]}]]
+
      [:h4 "Generated Citation"]
      [:p "Nulla posuere.  Praesent augue."]
 

851395979f0742bfc6e605644d3cf22c8759e08f
Joshua Groombridge <joshua@condense.com.au>
2022-02-17 12:17:26 +1100

    (SD-33) show lodge error if there are errors in the form

--- frontend/src/cljs/metcalf/tern/config.cljs
+++ frontend/src/cljs/metcalf/tern/config.cljs
@@ -2130,7 +2130,10 @@
      [:h2 "10: Lodge Metadata Draft"]
      [:p "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]
 
-     [:div
+     [m4/when-errors
+      {:form-id   [:form]
+       :data-path []}
+      
       ;; TODO: CSS for <b> styling?
       [:b
        {:style {:color "#e36a51"}}

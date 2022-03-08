2ccfb3effb0a62a64629833e938376ec7d714f7e
Mark Hepburn <Mark.Hepburn@gmail.com>
2022-03-08 13:28:30 +1100

    SD-37: Conditionally append custom citation details.

--- frontend/src/cljs/metcalf/common/rules4.cljs
+++ frontend/src/cljs/metcalf/common/rules4.cljs
@@ -464,7 +464,7 @@
     "{Identifier}"))
 
 (defn -format-citation
-  [{:keys [title date dateSubmitted authors coauthors version doi]}]
+  [{:keys [title date dateSubmitted authors coauthors version customCitation doi]}]
   (let [date (cljs-time/value-to-date (or date dateSubmitted))
         year (.getFullYear (or date (js/Date.))) ; fallback for (dev-only?) case where it hasn't been saved yet
         authors (map -format-author authors)
@@ -474,11 +474,13 @@
                          (apply str))]
     (str author-list " (" year "): Version " version ". " title
          ". Terrestrial Ecosystem Research Network (TERN). (Dataset). "
-         (-format-doi doi))))
+         (-format-doi doi)
+         (when-not (contains? empty-values customCitation)
+           (str ". " customCitation)))))
 
 (defn generate-citation
   [block]
-  (let [{:strs [title date dateSubmitted doi citedResponsibleParty version]} (blocks4/as-data block)
+  (let [{:strs [title date dateSubmitted doi citedResponsibleParty version customCitation]} (blocks4/as-data block)
         authors (->> citedResponsibleParty
                      (filter #(= "a37cc120-9920-4495-9a2f-698e225b5902"
                                  (get-in % ["role" "UUID"]))))
@@ -487,11 +489,12 @@
                                    (get-in % ["role" "UUID"]))))]
     (assoc-in block [:content "generatedCitation" :props :value]
               (-format-citation
-               {:authors       authors
-                :coauthors     coauthors
-                :title         title
-                :date          date
-                :dateSubmitted dateSubmitted
-                :version       version
-                :doi           doi}))))
+               {:authors        authors
+                :coauthors      coauthors
+                :title          title
+                :date           date
+                :dateSubmitted  dateSubmitted
+                :version        version
+                :doi            doi
+                :customCitation customCitation}))))
 

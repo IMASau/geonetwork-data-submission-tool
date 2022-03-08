0379e597607074e1d74c62ea0ff19dae8e9166c1
Mark Hepburn <Mark.Hepburn@gmail.com>
2022-03-08 16:25:06 +1100

    SD-110: Tooltips: Lodge tab

--- frontend/src/cljs/metcalf/tern/config.cljs
+++ frontend/src/cljs/metcalf/tern/config.cljs
@@ -2086,7 +2086,7 @@ recorded in the standard citation."
     :lodge
     [:div
      [:h2 "10: Lodge Metadata Draft"]
-     [:p "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]
+     [:p "Information about distribution and options to load data."]
 
      [m4/when-errors
       {:form-id   [:form]

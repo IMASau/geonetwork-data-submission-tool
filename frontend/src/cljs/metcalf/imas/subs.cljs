4d330acceb454a98a0a8418a659b377a966b290a
Mark Hepburn <Mark.Hepburn@gmail.com>
2022-03-16 15:27:28 +1100

    IWA-820: start porting edit-tabs functionality

--- frontend/src/cljs/metcalf/imas/subs.cljs
+++ frontend/src/cljs/metcalf/imas/subs.cljs
@@ -1,4 +1,5 @@
-(ns metcalf.imas.subs)
+(ns metcalf.imas.subs 
+  (:require [metcalf.common.blocks4 :as blocks4]))
 
 (def edit-tabs
   "Default edit tabs for imas deploy"
@@ -17,6 +18,11 @@
   (let [successors (drop-while #(not= tab-id (:id %)) edit-tabs)]
     (second successors)))
 
+(defn- get-next-tab2
+  [tab-id edit-tabs]
+  (let [successors (drop-while #(not= tab-id (:id %)) edit-tabs)]
+    (second successors)))
+
 (defn get-edit-tab-props
   [[page form-state]]
   (let [selected-tab (get page :tab :data-identification)]
@@ -30,3 +36,24 @@
                           :title       text
                           :has-errors? (and error-count (> error-count 0))}))
                      edit-tabs)}))
+
+(defn get-edit-tab-props2
+  "Sub to return edit-tab props for use in views.
+   Returns selected-tab and tab-props.
+   Each tab-prop includes an id, title and has-error? flag"
+  [[page form-state edit-tabs]]
+  (letfn [(has-block-errors? [data-path]
+            (let [block (get-in form-state (blocks4/block-path data-path))]
+              (get-in block [:progress/score :progress/errors])))]
+    (let [selected-tab (get page :tab :data-identification)]
+      {:selected-tab selected-tab
+       :next-tab     (get-next-tab2 selected-tab edit-tabs)
+       :tab-props    (mapv
+                       (fn [{:keys [id text data-paths]}]
+                         (let [has-errors? (some has-block-errors? data-paths)
+                               has-required-fields? (seq data-paths)]
+                           {:id                   id
+                            :title                text
+                            :has-errors?          (boolean has-errors?)
+                            :has-required-fields? (boolean has-required-fields?)}))
+                       edit-tabs)})))

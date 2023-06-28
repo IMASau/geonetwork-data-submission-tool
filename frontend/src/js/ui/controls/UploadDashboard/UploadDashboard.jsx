dd1e18e4032a1314c8ff18bb0f01094a9abd3832
Joshua Groombridge <joshua@condense.com.au>
2023-06-28 15:01:57 +1000

    local & cloud upload split WIP

--- frontend/src/js/ui/controls/UploadDashboard/UploadDashboard.jsx
+++ frontend/src/js/ui/controls/UploadDashboard/UploadDashboard.jsx
@@ -12,7 +12,7 @@ export default class UploadDashboard extends React.Component {
         super(props);
         const { tusUrl, xhrUrl, companionUrl, csrf, onUploadSuccess, metadata } = props;
 
-        this.uppy = new Uppy({
+        const uppy = new Uppy({
             id: 'uppy',
             autoProceed: true,
             debug: true,
@@ -22,14 +22,34 @@ export default class UploadDashboard extends React.Component {
                         file.meta[key] = value;
                     }
                 }
+
+                const source = Object.values(files)[0].source;
+                if (source == 'react:Dashboard') {
+                    uppy.removePlugin(uppy.getPlugin('Tus'));
+                } else {
+                    uppy.removePlugin(uppy.getPlugin('XHRUpload'));
+                }
             }
         })
+            .on(
+                'complete', result => {
+                    const source = result.successful.concat(result.failed)[0].source;
+                    console.log(source);
+                    if (source == 'react:Dashboard') {
+                        uppy.use(Tus, { endpoint: tusUrl });
+                    } else {
+                        uppy.use(XHR, { endpoint: xhrUrl, formData: true, headers: { 'X-CSRFToken': csrf } });
+                    }
+                }
+            )
             .use(Tus, { endpoint: tusUrl })
-            .use(XHR, { endpoint: xhrUrl, formData: true, headers: {'X-CSRFToken': csrf} })
+            .use(XHR, { endpoint: xhrUrl, formData: true, headers: { 'X-CSRFToken': csrf } })
             .use(GoogleDrive, { companionUrl: companionUrl })
             .use(Url, { companionUrl: companionUrl })
             .use(ASDC, { companionUrl: companionUrl })
             .on('upload-success', onUploadSuccess);
+
+        this.uppy = uppy;
     }
 
     componentWillUnmount() {
